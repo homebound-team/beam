c93336df5abb63dd698a15bdf6257a8e8f3f5214
Kolton <Kolton.Gagnon@Gmail.com>
2021-03-01 17:00:32 -0400

    Add conventional commit and circleci support

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+};
