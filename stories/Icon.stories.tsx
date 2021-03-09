b1376f44be78bc7378199c4de58ee8fbc595b447
Stephen Haberman <shaberman@homebound.com>
2021-03-09 15:20:02 -0600

    Use babel-preset-css-prop instead of per-file pragmas.

--- stories/Icon.stories.tsx
+++ stories/Icon.stories.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { Meta } from '@storybook/react';
-import { Icon as IconComponent, IconProps, Icons } from './';
+import { Icon as IconComponent, IconProps, Icons } from '../';
 
 export default {
   title: 'Icon',
