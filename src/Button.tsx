c790b4686c87ff07ba3ac6d63e4408fb5eb55907
Brandon Dow <bdow@homebound.com>
2021-03-12 00:31:47 -0500

    Use 'useFocusRing' hook instead of wrapping component

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,4 +1,4 @@
-import { useButton, FocusRing } from "react-aria"
+import { useButton, useFocusRing } from "react-aria"
 import type { AriaButtonProps } from "@react-types/button";
 import {useMemo, useRef} from "react";
 import { Css, Palette, px } from "./Css";
@@ -14,17 +14,17 @@ export function Button(props: ButtonProps) {
   const { children, icon, variant = "primary", size = "sm" } = props;
   const ref = useRef(null);
   const { buttonProps } = useButton(props, ref);
+  const { isFocusVisible, focusProps } = useFocusRing(props);
   const buttonStyles = useMemo(() => getButtonStyles(variant, size), [variant, size])
+  const focusRingStyles = useMemo(() => variant === "danger" ? dangerFocusRingStyles : defaultFocusRingStyles, [variant]);
 
   return (
-    <FocusRing focusRingClass="focusRing">
-      <button ref={ref} {...buttonProps} css={{...buttonReset, ...buttonStyles}}>
-        {icon && (
-          <Icon xss={iconStyles[size]} color="inherit" icon={icon} />
-        )}
-        {children}
-      </button>
-    </FocusRing>
+    <button ref={ref} {...buttonProps} {...focusProps} css={{...buttonReset, ...buttonStyles, ...(isFocusVisible ? focusRingStyles : {})}}>
+      {icon && (
+        <Icon xss={iconStyles[size]} color="inherit" icon={icon} />
+      )}
+      {children}
+    </button>
   );
 }
 
@@ -34,7 +34,7 @@ const buttonReset = Css.p0.bsNone.cursorPointer.smEm.m(px(4)).dif.itemsCenter
   .add("outline", "inherit")
   .add("borderRadius", px(4)).$;
 const disabledStyles = Css.add("cursor", "not-allowed").$;
-const focusRingStyles = Css.add("boxShadow", `0px 0px 0px 2px ${Palette.White}, 0 0 0 4px ${Palette.Sky500}`).$;
+const defaultFocusRingStyles = Css.add("boxShadow", `0px 0px 0px 2px ${Palette.White}, 0 0 0 4px ${Palette.Sky500}`).$;
 const dangerFocusRingStyles = Css.add("boxShadow", `0px 0px 0px 2px ${Palette.White}, 0 0 0 4px ${Palette.Coral600}`).$;
 
 const variantStyles: Record<ButtonVariant, {}> = {
@@ -42,7 +42,6 @@ const variantStyles: Record<ButtonVariant, {}> = {
     ...Css.bgSky500.white.add("fill", Palette.White).$,
     "&:hover:not(:disabled)": Css.bgSky700.$,
     "&:disabled": {...disabledStyles, ...Css.bgSky200.$},
-    "&.focusRing:not(:disabled)": focusRingStyles,
     "&:active:not(:disabled)": Css.bgSky900.$,
   },
 
@@ -50,7 +49,6 @@ const variantStyles: Record<ButtonVariant, {}> = {
     ...Css.bgWhite.bCoolGray300.bw1.ba.coolGray900.add("fill", Palette.CoolGray900).$,
     "&:hover:not(:disabled)": Css.bgCoolGray50.$,
     "&:disabled": {...disabledStyles, ...Css.bgWhite.coolGray300.add("fill", Palette.CoolGray300).$},
-    "&.focusRing:not(:disabled)": focusRingStyles,
     "&:active:not(:disabled)": Css.bgCoolGray200.$,
   },
 
@@ -58,7 +56,6 @@ const variantStyles: Record<ButtonVariant, {}> = {
     ...Css.add("background", "none").sky500.add("fill", Palette.Sky500).$,
     "&:hover:not(:disabled)": Css.bgCoolGray100.$,
     "&:disabled": {...disabledStyles, ...Css.coolGray300.add("fill", Palette.CoolGray300).$},
-    "&.focusRing:not(:disabled)": focusRingStyles,
     "&:active:not(:disabled)": Css.sky900.add("fill", Palette.Sky900).$,
   },
 
@@ -66,7 +63,6 @@ const variantStyles: Record<ButtonVariant, {}> = {
     ...Css.bgCoral600.white.add("fill", Palette.White).$,
     "&:hover:not(:disabled)": Css.bgCoral500.$,
     "&:disabled": {...disabledStyles, ...Css.bgCoral200.$},
-    "&.focusRing:not(:disabled)": dangerFocusRingStyles,
     "&:active:not(:disabled)": Css.bgCoral700.$,
   }
 }
