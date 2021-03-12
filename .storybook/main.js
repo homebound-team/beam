dfb0033b84b84f640202b8ce9cb85ca26be0c47f
Stephen Haberman <shaberman@homebound.com>
2021-03-12 14:05:06 -0600

    Fix fragments b/c order matters.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,7 @@ module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-  typescript: {
-    check: false,
-  },
+  typescript: { check: false },
   webpackFinal: async (config) => {
     // Make our `src/...` imports work
     config.resolve.modules.push(__dirname, "./");
