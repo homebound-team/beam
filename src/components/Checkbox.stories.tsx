c75710ac12107309db4305ccf0ece9eea11e3956
KSK <kskano@gmail.com>
2021-03-18 17:24:03 -0600

    Use VisuallyHidden component

--- src/components/Checkbox.stories.tsx
+++ src/components/Checkbox.stories.tsx
@@ -12,17 +12,16 @@ export function Checkboxes() {
     <div css={Css.df.flexColumn.$}>
       <div>
         <h2>No Label</h2>
-        <div css={Css.df.wPx(150).justifyBetween.pb1.$}>
-          <Checkbox aria-label="default"></Checkbox>
+        <div css={Css.df.wPx(120).justifyBetween.pb1.$}>
+          <Checkbox aria-label="default" autoFocus onChange={() => {}}></Checkbox>
           <Checkbox aria-label="selected" isSelected></Checkbox>
-          <Checkbox aria-label="focused" autoFocus isSelected></Checkbox>
           <Checkbox aria-label="indeterminate" isIndeterminate></Checkbox>
           <Checkbox aria-label="disabled" isDisabled></Checkbox>
         </div>
       </div>
       <h2>With Label</h2>
       <div css={Css.df.wPx(300).justifyBetween.pb1.$}>
-        <Checkbox>Label</Checkbox>
+        <Checkbox onChange={() => {}}>Label</Checkbox>
         <Checkbox isSelected>Label</Checkbox>
         <Checkbox isIndeterminate>Label</Checkbox>
         <Checkbox isDisabled>Label</Checkbox>
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { useCheckbox } from "@react-aria/checkbox";
 import { useFocusRing } from "@react-aria/focus";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
 import { useToggleState } from "@react-stately/toggle";
 import { AriaCheckboxProps } from "@react-types/checkbox";
 import { useRef } from "react";
@@ -8,40 +9,53 @@ import { Css, Palette } from "src/Css";
 export interface CheckboxProps extends AriaCheckboxProps {}
 
 export function Checkbox(props: CheckboxProps) {
-  const { children, isSelected, isIndeterminate, isDisabled } = props;
+  const { children, isIndeterminate = false, isDisabled = false } = props;
   const ref = useRef(null);
   const state = useToggleState(props);
+  const { isSelected } = state;
   const { inputProps } = useCheckbox(props, state, ref);
   const { isFocusVisible, focusProps } = useFocusRing(props);
-  const markIcon = isIndeterminate ? smallDash : SmallCheckmark;
+  const markIcon = isIndeterminate ? smallDash : isSelected ? SmallCheckmark : "";
 
   return (
     <label css={Css.df.itemsCenter.$}>
-      <input
-        ref={ref}
-        {...inputProps}
-        {...focusProps}
+      <VisuallyHidden>
+        <input ref={ref} {...inputProps} {...focusProps} />
+      </VisuallyHidden>
+      <span
         css={{
-          ...checkboxReset,
-          ...checkboxStyles,
-          ...((isSelected || isIndeterminate) && filledStyle),
+          ...checkboxStyles({ isDisabled, isSelected, isIndeterminate }),
           ...(isFocusVisible && focusRingStyles),
         }}
-      />
+        aria-hidden="true"
+      ></span>
       <span css={markStyles}>{markIcon}</span>
-      {children && <span css={Css.pl1.sm.if(!!isDisabled).coolGray300.$}>{children}</span>}
+      {children && <span css={labelStyles(isDisabled)}>{children}</span>}
     </label>
   );
 }
-const checkboxReset = Css.add("appearance", "none").add("boxSizing", "border-box").$;
-const checkboxStyles = { ...Css.m0.ba.bCoolGray300.br4.hPx(16).wPx(16).bgWhite.$, "&:disabled": Css.bgCoolGray300.$ };
-const filledStyle = Css.bgSky500.bSky500.$;
-const focusRingStyles = Css.outline0.add("boxShadow", `0px 0px 0px 2px ${Palette.White}, 0 0 0 4px ${Palette.Sky500}`)
-  .$;
+
+interface ICheckboxStyles {
+  isDisabled: boolean;
+  isSelected: boolean;
+  isIndeterminate: boolean;
+}
+
+function checkboxStyles({ isDisabled, isSelected, isIndeterminate }: ICheckboxStyles) {
+  return Css.add("boxSizing", "border-box")
+    .hPx(16)
+    .wPx(16)
+    .ba.bCoolGray300.br4.bgWhite.if(isSelected || isIndeterminate)
+    .bSky500.bgSky500.if(isDisabled).bCoolGray300.bgCoolGray100.$;
+}
+const focusRingStyles = Css.add("boxShadow", `0px 0px 0px 2px ${Palette.White}, 0 0 0 4px ${Palette.Sky500}`).$;
 const markStyles = { ...Css.relative.$, "& svg": Css.absolute.topPx(-8).rightPx(0).$ };
+function labelStyles(isDisabled: boolean) {
+  return Css.pl1.sm.if(isDisabled).coolGray300.$;
+}
 
 export const SmallCheckmark = (
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="16" height="16">
     <path
       d="M6.66669 10.3907L4.47135 8.19533L3.52869 9.138L6.66669 12.276L13.138 5.80467L12.1954 4.862L6.66669 10.3907Z"
       fill={Palette.White}
@@ -50,7 +64,7 @@ export const SmallCheckmark = (
 );
 
 const smallDash = (
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="16" height="16">
     <rect x="4" y="7.5" width="8" height="1.35" fill={Palette.White} />
   </svg>
 );
