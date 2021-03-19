9de6ff93fb7c8ea2bdfcf8f61eeb13d622d961b1
Stephen Haberman <shaberman@homebound.com>
2021-03-19 15:49:26 -0500

    Hook up aria ids.

---
+++ src/components/RadioGroupField.test.tsx
@@ -0,0 +1,38 @@
+import { render } from "@homebound/rtl-utils";
+import { RadioGroupField } from "src/components/RadioGroupField";
+
+describe("RadioGroupField", () => {
+  it("renders with just labels", async () => {
+    const r = await render(
+      <RadioGroupField
+        label={"Favorite cheese"}
+        value="a"
+        onChange={() => {}}
+        options={[
+          { label: "Asiago", value: "a" },
+          { label: "Burratta", value: "b" },
+        ]}
+      />,
+    );
+    expect(r.baseElement).toMatchSnapshot();
+  });
+
+  it("renders with just labels and description", async () => {
+    const r = await render(
+      <RadioGroupField
+        label={"Favorite cheese"}
+        value="a"
+        onChange={() => {}}
+        options={[
+          { value: "a", label: "Asiago" },
+          {
+            value: "b",
+            label: "Burratta",
+            description: "Burrata is an Italian cow milk cheese made from mozzarella and cream.",
+          },
+        ]}
+      />,
+    );
+    expect(r.baseElement).toMatchSnapshot();
+  });
+});
--- src/components/RadioGroupField.tsx
+++ src/components/RadioGroupField.tsx
@@ -5,26 +5,42 @@ import { Css, Palette } from "src/Css";
 
 export interface RadioFieldOption<K extends string> {
   // testId?: string;
+  /** The label for a specific option, i.e. "Cheddar". */
   label: string;
+  /** An optional longer description to render under the label. */
   description?: string | (() => ReactNode);
+  /** The undisplayed value, i.e. an id of some sort. */
   value: K;
 }
 
 interface RadioGroupFieldProps<K extends string> {
   // id?: string;
+  /** The label for the choice itself, i.e. "Favorite Cheese". */
   label: string;
+  /** The currently selected option value (i.e. an id). */
   value: K | undefined;
+  /** Called when an option is selected. We don't support unselecting. */
   onChange: (value: K) => void;
+  /** The list of options. */
   options: RadioFieldOption<K>[];
 }
 
+/**
+ * Provides a radio group with label.
+ *
+ * This is generally meant to be used in a form vs. being raw radio buttons.
+ */
 export function RadioGroupField<K extends string>(props: RadioGroupFieldProps<K>) {
   const { label, value, onChange, options } = props;
+
+  // Assume this is unique enough and more deterministic for tests than random ids
+  const name = idize(label);
+  const labelId = `${name}-label`;
 
   // Instead of calling useRadioState, adapt our props to it, so we can still use useRadioGroup
   const [lastFocusedValue, setLastFocusedValue] = useState<string | null>(null);
   const state: RadioGroupState = {
-    name: "foo",
+    name,
     isDisabled: false,
     isReadOnly: false,
     selectedValue: value || null,
@@ -34,19 +50,24 @@ export function RadioGroupField<K extends string>(props: RadioGroupFieldProps<K>
   };
 
   // We use useRadioGroup b/c it does neat keyboard up/down stuff
-  const { labelProps, radioGroupProps } = useRadioGroup({ label }, state);
+  // TOOD: Pass read only, required, disabled, error message to useRadioGroup
+  const { radioGroupProps } = useRadioGroup(
+    // Tell useRadioGroup to not generate unique ids for these
+    { id: name, name, "aria-labelledby": labelId },
+    state,
+  );
 
   // max-width is dependent on having descriptions
   const anyDescriptions = options.some((o) => !!o.description);
 
   return (
     <div css={Css.maxw(anyDescriptions ? "344px" : "320px").$}>
-      <div {...labelProps} css={Css.sm.coolGray500.my1.$}>
+      <div id={labelId} css={Css.sm.coolGray500.my1.$}>
         {label}
       </div>
       <div {...radioGroupProps}>
         {options.map((option) => (
-          <Radio option={option} state={state} />
+          <Radio key={option.value} parentId={name} option={option} state={state} />
         ))}
       </div>
     </div>
@@ -54,38 +75,43 @@ export function RadioGroupField<K extends string>(props: RadioGroupFieldProps<K>
 }
 
 // Not meant to be standalone, but its own component so it can use hooks
-function Radio<K extends string>(props: { option: RadioFieldOption<K>; state: RadioGroupState }) {
-  const { option, state } = props;
-  const ref = useRef<HTMLInputElement>(null);
-  const { inputProps } = useRadio(
-    {
-      value: option.value,
-      // We wrap option.label+description in a `label` element but only want the
-      // shorter option.label value to be the aria-label text.
-      "aria-label": option.label,
-    },
+function Radio<K extends string>(props: { parentId: string; option: RadioFieldOption<K>; state: RadioGroupState }) {
+  const {
+    parentId,
+    option: { description, label, value },
     state,
-    ref,
-  );
+  } = props;
+
+  const labelId = `${parentId}-${value}-label`;
+  const descriptionId = `${parentId}-${value}-description`;
+  const ref = useRef<HTMLInputElement>(null);
+  const { inputProps } = useRadio({ value, "aria-labelledby": labelId }, state, ref);
 
   return (
-    <label key={option.value} css={Css.df.cursorPointer.mb1.$}>
+    <label css={Css.df.cursorPointer.mb1.$}>
       <input
         type="radio"
         ref={ref}
         css={{
           ...radioReset,
-          ...(state.selectedValue === option.value ? radioChecked : radioUnchecked),
+          ...(state.selectedValue === value ? radioChecked : radioUnchecked),
           // Sometimes we use useFocusRing, but here we want both to focus to drive
           ...{ "&:focus": radioFocus },
           // Nudge down so the center of the circle lines up with the label text
           ...Css.mtPx(2).mr1.$,
         }}
         {...inputProps}
+        aria-labelledby={labelId}
       />
       <div>
-        <div css={Css.smEm.coolGray700.$}>{option.label}</div>
-        <div css={Css.sm.coolGray500.$}>{option.description}</div>
+        <div id={labelId} css={Css.smEm.coolGray700.$} {...(description ? { "aria-describedby": descriptionId } : {})}>
+          {label}
+        </div>
+        {description && (
+          <div id={descriptionId} css={Css.sm.coolGray500.$}>
+            {description}
+          </div>
+        )}
       </div>
     </label>
   );
@@ -130,3 +156,14 @@ export const radioFocus = {
   // Draw 1st box shadow of white/outline, 2nd box current (blue) of another outline
   ...Css.add("boxShadow", `${Palette.White} 0px 0px 0px 2px, currentColor 0px 0px 0px 4px`).$,
 };
+
+/**
+ * Given a label, makes a "probably good/unique" DOM id.
+ *
+ * The gist is to provide a better developer UX than always repeating
+ * `<Component id=foo label=Foo />` when ~90% of the time we can guess
+ * the id.
+ */
+function idize(label: string): string {
+  return label.replace(/[^a-zA-Z0-9]/, "").toLowerCase();
+}
