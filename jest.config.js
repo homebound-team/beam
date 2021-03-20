ddd218213263b3e4a1b3e576df74470275f3f34c
Stephen Haberman <shaberman@homebound.com>
2021-03-20 14:56:24 -0500

    Fix jest.

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testMatch: ["<rootDir>/src/**/*.test.{ts,tsx,js,jsx}"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.tsx"],
   moduleNameMapper: {
-    "^src/(.*)": "<rootDir>/src/$1",
+    "^src(.*)": "<rootDir>/src$1",
   },
   snapshotSerializers: ["@emotion/jest/serializer"],
 };
