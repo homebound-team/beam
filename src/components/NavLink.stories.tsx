ca04a323fad81166228b5931a4fd070194db6ba8
Kimiko Kano <ksk5280@users.noreply.github.com>
2021-04-08 11:52:04 -0600

    feat: Add NavLink component (#43)
    * Add NavLink component and story
    
    * Add external props, pressed styles,
    skip disable focus
    
    * Add base states story
    
    * Switch to useButton for isPressed styles
    
    * Add comments

---
+++ src/components/NavLink.stories.tsx
@@ -0,0 +1,114 @@
+import { Meta } from "@storybook/react";
+import { Fragment } from "react";
+import { navLink } from "src/components";
+import { Css } from "src/Css";
+import { getNavLinkStyles, NavLink, NavLinkProps } from "src/index";
+import { Icon, Icons } from "./Icon";
+
+export default {
+  title: "Components/NavLinks",
+  component: NavLink,
+  args: {
+    href: "",
+    label: "Nav link",
+    variant: "side",
+  },
+  argTypes: {
+    icon: { control: { type: "select", options: Object.keys(Icons) } },
+  },
+  parameters: {
+    // To better view the icon hover state
+    backgrounds: { default: "white" },
+  },
+} as Meta;
+
+export function BaseStates() {
+  const sideNavStyles = getNavLinkStyles("side");
+  const globalNavStyles = getNavLinkStyles("global");
+  const args = { href: "", className: navLink };
+
+  return (
+    <div css={Css.df.gap4.$}>
+      <div css={Css.df.flexColumn.gap2.$}>
+        <a {...args} css={sideNavStyles.baseStyles}>
+          {getChildren("Side nav default")}
+        </a>
+        <a {...args} css={{ ...sideNavStyles.baseStyles, ...sideNavStyles.hoverStyles }}>
+          {getChildren("Side nav hovered")}
+        </a>
+        <a {...args} css={{ ...sideNavStyles.baseStyles, ...sideNavStyles.pressedStyles }}>
+          {getChildren("Side nav pressed")}
+        </a>
+        <a {...args} css={{ ...sideNavStyles.baseStyles, ...sideNavStyles.activeStyles }}>
+          {getChildren("Side nav active")}
+        </a>
+        <a {...args} css={{ ...sideNavStyles.baseStyles, ...sideNavStyles.focusStyles }}>
+          {getChildren("Side nav focused")}
+        </a>
+        <a {...args} css={{ ...sideNavStyles.baseStyles, ...sideNavStyles.disabledStyles }}>
+          {getChildren("Side nav disabled")}
+        </a>
+      </div>
+      <div css={Css.df.flexColumn.gap2.$}>
+        <a {...args} css={globalNavStyles.baseStyles}>
+          {getChildren("Global nav default")}
+        </a>
+        <a {...args} css={{ ...globalNavStyles.baseStyles, ...globalNavStyles.hoverStyles }}>
+          {getChildren("Global nav hovered")}
+        </a>
+        <a {...args} css={{ ...globalNavStyles.baseStyles, ...globalNavStyles.pressedStyles }}>
+          {getChildren("Global nav pressed")}
+        </a>
+        <a {...args} css={{ ...globalNavStyles.baseStyles, ...globalNavStyles.activeStyles }}>
+          {getChildren("Global nav active")}
+        </a>
+        <a {...args} css={{ ...globalNavStyles.baseStyles, ...globalNavStyles.focusStyles }}>
+          {getChildren("Global nav focused")}
+        </a>
+        <a {...args} css={{ ...globalNavStyles.baseStyles, ...globalNavStyles.disabledStyles }}>
+          {getChildren("Global nav disabled")}
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function getChildren(label: string) {
+  return (
+    <Fragment>
+      {label}
+      <Icon icon="linkExternal" css={Css.ml1.$} />
+    </Fragment>
+  );
+}
+
+export const NavLinks = (args: NavLinkProps) => {
+  return (
+    <div css={Css.df.gap2.$}>
+      <div css={Css.df.flexColumn.gap2.$}>
+        <h2>Side nav link</h2>
+        <NavLink {...args} icon={undefined} />
+        <NavLink {...args} icon={undefined} active />
+        <NavLink {...args} icon={undefined} disabled />
+      </div>
+      <div css={Css.df.flexColumn.gap2.$}>
+        <h2>Side nav link with icon</h2>
+        <NavLink icon="linkExternal" {...args} />
+        <NavLink icon="linkExternal" {...args} active />
+        <NavLink icon="linkExternal" {...args} disabled />
+      </div>
+      <div css={Css.df.flexColumn.gap2.$}>
+        <h2>Global nav link</h2>
+        <NavLink {...args} icon={undefined} variant="global" />
+        <NavLink {...args} icon={undefined} variant="global" active />
+        <NavLink {...args} icon={undefined} variant="global" disabled />
+      </div>
+      <div css={Css.df.flexColumn.gap2.$}>
+        <h2>Global nav link with icon</h2>
+        <NavLink icon="linkExternal" {...args} variant="global" />
+        <NavLink icon="linkExternal" {...args} variant="global" active />
+        <NavLink icon="linkExternal" {...args} variant="global" disabled />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/NavLink.tsx
@@ -0,0 +1,97 @@
+import { RefObject, useMemo, useRef } from "react";
+import { useButton, useFocusRing, useHover } from "react-aria";
+import { navLink } from "src/components";
+import { Css } from "src/Css";
+import { BeamFocusableProps } from "src/interfaces";
+import { Icon, Icons } from "./Icon";
+
+export interface NavLinkProps extends BeamFocusableProps {
+  /** active indicates the user is on the current page */
+  active?: boolean;
+  disabled?: boolean;
+  href: string;
+  label: string;
+  icon?: keyof typeof Icons;
+  variant: NavLinkVariant;
+  /**
+   * Target and rel attributes are added for external links
+   * TODO: maybe infer this property based on the href */
+  external?: boolean;
+}
+
+type NavLinkVariant = "side" | "global";
+
+export function NavLink(props: NavLinkProps) {
+  const { disabled: isDisabled, label, external, ...otherProps } = props;
+  const ariaProps = { children: label, isDisabled, ...otherProps };
+  const { href, active = false, icon = false, variant } = ariaProps;
+  const ref = useRef() as RefObject<HTMLAnchorElement>;
+  const { buttonProps, isPressed } = useButton({ ...ariaProps, elementType: "a" }, ref);
+  const { hoverProps, isHovered } = useHover({ isDisabled });
+  const { isFocusVisible, focusProps } = useFocusRing(ariaProps);
+
+  const { baseStyles, activeStyles, focusStyles, hoverStyles, disabledStyles, pressedStyles } = useMemo(
+    () => getNavLinkStyles(variant),
+    [variant],
+  );
+
+  const target = external ? "_blank" : undefined;
+  const rel = external ? "noopener noreferrer" : undefined;
+
+  return (
+    <a
+      {...buttonProps}
+      {...focusProps}
+      {...hoverProps}
+      className={navLink}
+      href={href}
+      ref={ref}
+      rel={rel}
+      // does not focus if disabled
+      tabIndex={isDisabled ? -1 : 0}
+      target={target}
+      css={{
+        ...baseStyles,
+        ...(active && activeStyles),
+        ...(isDisabled && disabledStyles),
+        ...(isFocusVisible && focusStyles),
+        ...(isHovered && hoverStyles),
+        ...(isPressed && pressedStyles),
+      }}
+    >
+      <span css={Css.mr1.$}>{label}</span>
+      {icon && <Icon icon={icon} />}
+    </a>
+  );
+}
+
+export function getNavLinkStyles(variant: NavLinkVariant) {
+  return navLinkVariantStyles[variant];
+}
+
+const baseStyles = Css.df.itemsCenter.hPx(32).pyPx(6).px1.br4.smEm.outline0.$;
+
+const navLinkVariantStyles: Record<
+  NavLinkVariant,
+  { baseStyles: {}; hoverStyles: {}; disabledStyles: {}; focusStyles: {}; activeStyles: {}; pressedStyles: {} }
+> = {
+  side: {
+    baseStyles: { ...baseStyles, ...Css.wPx(184).gray700.$ },
+    activeStyles: Css.lightBlue700.bgLightBlue50.$,
+    disabledStyles: Css.gray400.cursorNotAllowed.$,
+    focusStyles: Css.bgLightBlue50.bshFocus.$,
+    hoverStyles: Css.gray700.bgGray100.$,
+    pressedStyles: Css.gray700.bgGray200.$,
+  },
+  global: {
+    baseStyles: { ...baseStyles, ...Css.add("width", "max-content").gray500.$ },
+    activeStyles: Css.white.bgGray900.$,
+    disabledStyles: Css.gray400.cursorNotAllowed.$,
+    focusStyles: Css.gray500.bgGray900.add(
+      "boxShadow",
+      `0px 1px 2px rgba(0, 0, 0, 0.05), 0px 0px 0px 2px #242424, 0px 0px 0px 4px #0EA5E9`,
+    ).$,
+    hoverStyles: Css.gray500.bgGray900.$,
+    pressedStyles: Css.gray500.bgGray700.$,
+  },
+};
