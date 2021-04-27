47f7975ada8c347ec6a12e0fb8f1a66aa10e61f0
Kimiko Kano <ksk5280@users.noreply.github.com>
2021-04-27 16:41:36 -0600

    feat: Compact button groups (#59)

--- src/components/ButtonGroup.stories.tsx
+++ src/components/ButtonGroup.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta } from "@storybook/react";
+import { Css } from "src/Css";
 import { ButtonGroup } from "src/index";
 
 export default {
@@ -8,39 +9,30 @@ export default {
 
 export function ButtonGroups() {
   return (
-    <div>
+    <div css={Css.df.flexColumn.childGap2.$}>
       <div>
         <h2>Icon Only</h2>
-        <ButtonGroup buttons={[
-          {icon: "chevronLeft"},
-          {icon: "plus"},
-          {icon: "chevronRight"},
-          ]}/>
+        <ButtonGroup buttons={[{ icon: "chevronLeft" }, { icon: "plus" }, { icon: "chevronRight" }]} />
+        <ButtonGroup size="md" buttons={[{ icon: "chevronLeft" }, { icon: "plus" }, { icon: "chevronRight" }]} />
       </div>
       <div>
         <h2>Basic</h2>
-        <ButtonGroup buttons={[
-          {text: "Leading"},
-          {text: "Middle"},
-          {text: "Trailing"},
-        ]}/>
+        <ButtonGroup buttons={[{ text: "Leading" }, { text: "Middle" }, { text: "Trailing" }]} />
+        <ButtonGroup size="md" buttons={[{ text: "Leading" }, { text: "Middle" }, { text: "Trailing" }]} />
       </div>
       <div>
         <h2>Active</h2>
-        <ButtonGroup buttons={[
-          {text: "Leading", active: true },
-          {text: "Middle"},
-          {text: "Trailing"},
-        ]}/>
+        <ButtonGroup buttons={[{ text: "Leading", active: true }, { text: "Middle" }, { text: "Trailing" }]} />
+        <ButtonGroup
+          size="md"
+          buttons={[{ text: "Leading", active: true }, { text: "Middle" }, { text: "Trailing" }]}
+        />
       </div>
       <div>
         <h2>Disabled</h2>
-        <ButtonGroup disabled buttons={[
-          {text: "Leading"},
-          {text: "Middle"},
-          {text: "Trailing"},
-        ]}/>
+        <ButtonGroup disabled buttons={[{ text: "Leading" }, { text: "Middle" }, { text: "Trailing" }]} />
+        <ButtonGroup size="md" disabled buttons={[{ text: "Leading" }, { text: "Middle" }, { text: "Trailing" }]} />
       </div>
     </div>
-  )
+  );
 }
