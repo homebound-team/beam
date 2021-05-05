2422b9eab336e72499b77e3ed53570f837546b75
Stephen Haberman <shaberman@homebound.com>
2021-05-05 17:35:08 -0500

    Use useTestIds to pick up existing data-testid if passed.

--- src/forms/BoundTextField.tsx
+++ src/forms/BoundTextField.tsx
@@ -1,6 +1,7 @@
 import { FieldState } from "@homebound/form-state";
 import { Observer } from "mobx-react";
 import { TextField, TextFieldProps } from "src/components";
+import { useTestIds } from "src/utils";
 
 export type BoundTextFieldProps = Omit<TextFieldProps, "value"> & {
   field: FieldState<string | null | undefined>;
@@ -9,17 +10,18 @@ export type BoundTextFieldProps = Omit<TextFieldProps, "value"> & {
 /** Wraps `TextField` and binds it to a form field. */
 export function BoundTextField(props: BoundTextFieldProps) {
   const { field, readOnly, onChange = (value) => field.set(value), label = field.key, ...others } = props;
+  const testId = useTestIds(props, field.key);
   return (
     <Observer>
       {() => (
         <TextField
-          data-testid={field.key}
           label={label}
           value={field.value || undefined}
           onChange={onChange}
           readOnly={readOnly ?? field.readOnly}
           errorMsg={field.touched ? field.errors.join(" ") : undefined}
           onBlur={() => field.blur()}
+          {...testId}
           {...others}
         />
       )}
