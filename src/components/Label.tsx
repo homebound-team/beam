eb7a825db572964ed007f9081aa2fd4c4f43a878
Stephen Haberman <stephen.haberman@gmail.com>
2021-05-07 17:40:30 -0500

    fix: Export the form components, extract ErrorMessage. (#69)
    * fix: Export the form components, extract ErrorMessage.
    
    * Forgot to add ErrorMessage.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,9 +2,12 @@ import React, { LabelHTMLAttributes } from "react";
 import { Css } from "src/Css";
 
 interface LabelProps {
+  // We don't usually have `fooProps`-style props, but this is for/from react-aria
   labelProps?: LabelHTMLAttributes<HTMLLabelElement>;
   label: string;
 }
+
+/** An internal helper component for rendering form labels. */
 export function Label({ labelProps, label, ...others }: LabelProps) {
   return (
     <label {...labelProps} {...others} css={Css.sm.gray700.mbPx(4).$}>
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./components";
 export * from "./Css";
+export * from "./forms";
 export * from "./hooks";
 export * from "./interfaces";
 export * from "./utils/useTestIds";
