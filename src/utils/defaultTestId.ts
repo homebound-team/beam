d51a73b6d6a697491c4e7f7d4494c412a35798bf
Stephen Haberman <shaberman@homebound.com>
2021-05-11 11:35:34 -0500

    Give an example for defaultTestId.

--- src/utils/defaultTestId.ts
+++ src/utils/defaultTestId.ts
@@ -1,9 +1,10 @@
 import { camelCase } from "change-case";
 
 /**
- * Guesses an id based on a label string.
+ * Guesses an id based on a label string, i.e. given `Homeowner Contract`,
+ * returns `homeownerContract`.
  *
- * This is useful for our form fields that will probably have a label,
+ * This is useful for our (non-bound) form fields that will probably have a label,
  * but may not have an data-testid set by the encompassing page.
  */
 export function defaultTestId(label: string): string {
