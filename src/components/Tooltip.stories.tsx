cd9505af8ce1cd92cb87a72489a1268d3635532a
oagboola <lydexmail@yahoo.com>
2021-05-31 21:26:28 +0100

    add tooltip component

--- src/components/Tooltip.stories.tsx
+++ src/components/Tooltip.stories.tsx
@@ -12,7 +12,7 @@ export function Tooltip() {
   return (
     <div css={Css.w25.ml("25%").$}>
       {placements.map((placement, i) => (
-        <TooltipComponent title="Tooltip Info" placement={placement} key={i} delay={750}>
+        <TooltipComponent title="Tooltip Info" placement={placement} key={i}>
           <span css={Css.db.tc.my5.bgGray400.br4.$}>
             This tooltip is positioned at: <span css={Css.b.$}>{placement}</span>
           </span>
