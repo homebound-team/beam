eb1dade761dea641408889a39f292251361b8e69
Brandon <bdow@homebound.com>
2021-06-24 13:31:26 -0400

    fix: Remove margin from button styles (#133)
    * fix: Remove margin from button styles
    
    Removing a 4px margin from button styles which was initially used to reserve space for the FocusRing. Though, that has created alignment issues with other content. It is now expected that implementers of the Button component should account for the space needed for the focus ring.
    
    * Fix side-effects due to removing Button margin

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -21,31 +21,32 @@ export default {
 } as Meta<ButtonProps>;
 
 export function Buttons(args: ButtonProps) {
+  const buttonRowStyles = Css.df.childGap1.my1.$;
   return (
     <div css={Css.dg.flexColumn.childGap2.$}>
       <div>
         <h2>Primary</h2>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} label="Primary Button" autoFocus />
           <Button {...args} disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="md" label="Primary Button" />
           <Button {...args} size="md" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="lg" label="Primary Button" />
           <Button {...args} size="lg" disabled {...args} label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="plus" label="Primary Button" />
           <Button {...args} disabled icon="plus" label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="md" icon="plus" label="Primary Button" />
           <Button {...args} size="md" disabled icon="plus" label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="lg" icon="plus" label="Primary Button" />
           <Button {...args} size="lg" disabled icon="plus" label="Disabled" />
         </div>
@@ -53,27 +54,27 @@ export function Buttons(args: ButtonProps) {
 
       <div>
         <h2>Secondary</h2>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} variant="secondary" label="Secondary Button" />
           <Button {...args} variant="secondary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="md" variant="secondary" label="Secondary Button" />
           <Button {...args} size="md" variant="secondary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="lg" variant="secondary" label="Secondary Button" />
           <Button {...args} size="lg" variant="secondary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="plus" variant="secondary" label="Secondary Button" />
           <Button {...args} icon="plus" variant="secondary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="plus" size="md" variant="secondary" label="Secondary Button" />
           <Button {...args} icon="plus" size="md" variant="secondary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="plus" size="lg" variant="secondary" label="Secondary Button" />
           <Button {...args} icon="plus" size="lg" variant="secondary" disabled label="Disabled" />
         </div>
@@ -81,19 +82,19 @@ export function Buttons(args: ButtonProps) {
 
       <div>
         <h2>Tertiary</h2>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} variant="tertiary" label="Tertiary Button" />
           <Button {...args} variant="tertiary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="md" variant="tertiary" label="Tertiary Button" />
           <Button {...args} size="md" variant="tertiary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="plus" variant="tertiary" label="Tertiary Button" />
           <Button {...args} icon="plus" variant="tertiary" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="plus" size="md" variant="tertiary" label="Tertiary Button" />
           <Button {...args} icon="plus" size="md" variant="tertiary" disabled label="Disabled" />
         </div>
@@ -101,27 +102,27 @@ export function Buttons(args: ButtonProps) {
 
       <div>
         <h2>Danger!</h2>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} variant="danger" label="Danger Button" />
           <Button {...args} variant="danger" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="md" variant="danger" label="Danger Button" />
           <Button {...args} size="md" variant="danger" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} size="lg" variant="danger" label="Danger Button" />
           <Button {...args} size="lg" variant="danger" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="trash" variant="danger" label="Danger Button" />
           <Button {...args} icon="trash" variant="danger" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="trash" size="md" variant="danger" label="Danger Button" />
           <Button {...args} icon="trash" size="md" variant="danger" disabled label="Disabled" />
         </div>
-        <div>
+        <div css={buttonRowStyles}>
           <Button {...args} icon="trash" size="lg" variant="danger" label="Danger Button" />
           <Button {...args} icon="trash" size="lg" variant="danger" disabled label="Disabled" />
         </div>
