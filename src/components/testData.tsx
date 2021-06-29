227e77b5016752f06a3a4ad87e2f8538cafa0cb1
Stephen Haberman <stephen.haberman@gmail.com>
2021-06-29 09:32:58 -0500

    feat: Allow Tab values to be string literals. (#143)
    * feat: Allow Tab values to be string literals.
    
    * Rename T to V for value.

--- src/components/testData.tsx
+++ src/components/testData.tsx
@@ -1,6 +1,8 @@
 import { Tab } from "./Tabs";
 
-export const testTabs: Tab[] = [
+export type TabValue = "tab1" | "tab2" | "tab3" | "tab4";
+
+export const testTabs: Tab<TabValue>[] = [
   { name: "Tab 1", value: "tab1", render: () => <TabContent title="Tab 1 Content" /> },
   { name: "Tab 2", value: "tab2", render: () => <TabContent title="Tab 2 Content" /> },
   { name: "Tab 3", value: "tab3", disabled: true, render: () => <TabContent title="Tab 3 Content" /> },
