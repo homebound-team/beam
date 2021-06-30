9044d91d4814d67976997e9f6c0909c59c87f15b
Brandon Dow <bdow@homebound.com>
2021-06-30 09:49:17 -0400

    feat: ToggleChips
    Expected use for ToggleChips will be used in the Filter's modal. Design had named them FilterChips, however we are naming them ambiguous to the location they were originally designed for in case we want to reuse them elsewhere.
    
    Added an option to have the label for a field be hidden.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,19 +1,22 @@
 import React, { LabelHTMLAttributes } from "react";
+import { VisuallyHidden } from "react-aria";
 import { Css } from "src/Css";
 
 interface LabelProps {
   // We don't usually have `fooProps`-style props, but this is for/from react-aria
   labelProps?: LabelHTMLAttributes<HTMLLabelElement>;
   label: string;
+  hidden?: boolean;
 }
 
 /** An internal helper component for rendering form labels. */
-export function Label({ labelProps, label, ...others }: LabelProps) {
-  return (
+export function Label({ labelProps, label, hidden, ...others }: LabelProps) {
+  const labelEl = (
     <label {...labelProps} {...others} css={Css.dib.sm.gray700.mbPx(4).$}>
       {label}
     </label>
   );
+  return hidden ? <VisuallyHidden>{labelEl}</VisuallyHidden> : labelEl;
 }
 
 /** Used for showing labels within text fields. */
---
+++ src/inputs/ToggleChipGroup.tsx
@@ -0,0 +1,76 @@
+import { useCheckboxGroup } from "@react-aria/checkbox";
+import { CheckboxGroupState, useCheckboxGroupState } from "@react-stately/checkbox";
+import { useRef } from "react";
+import { useCheckboxGroupItem, useFocusRing, VisuallyHidden } from "react-aria";
+import { Label } from "src/components/Label";
+import { Css } from "src/Css";
+
+type ToggleChipItemProps = {
+  label: string;
+  value: string;
+};
+
+interface ToggleChipGroupProps {
+  label: string;
+  options: ToggleChipItemProps[];
+  values: string[];
+  onChange: (values: string[]) => void;
+  hideLabel?: boolean;
+}
+
+export function ToggleChipGroup(props: ToggleChipGroupProps) {
+  const { values, label, options, hideLabel } = props;
+  const state = useCheckboxGroupState({ ...props, value: values });
+  const { groupProps, labelProps } = useCheckboxGroup(props, state);
+
+  return (
+    <div {...groupProps} css={Css.relative.$}>
+      <Label label={label} {...labelProps} hidden={hideLabel} />
+      <div>
+        {options.map((o) => (
+          <ToggleChip
+            key={o.value}
+            value={o.value}
+            groupState={state}
+            selected={state.value.includes(o.value)}
+            label={o.label}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+interface ToggleChipProps {
+  label: string;
+  value: string;
+  groupState: CheckboxGroupState;
+  selected: boolean;
+}
+
+function ToggleChip(props: ToggleChipProps) {
+  const { label, value, groupState, selected: isSelected } = props;
+  const ref = useRef(null);
+  const { inputProps } = useCheckboxGroupItem({ value, "aria-label": label }, groupState, ref);
+  const { isFocusVisible, focusProps } = useFocusRing();
+
+  return (
+    <label
+      css={{
+        ...Css.relative.dib.br16.sm.px1.cursorPointer.pyPx(4).mr1.mb1.bgGray200.$,
+        ...(isSelected
+          ? {
+              ...Css.white.bgLightBlue700.$,
+              ":hover": Css.bgLightBlue800.$,
+            }
+          : { ":hover": Css.bgGray300.$ }),
+        ...(isFocusVisible ? Css.bshFocus.$ : {}),
+      }}
+    >
+      <VisuallyHidden>
+        <input {...inputProps} {...focusProps} />
+      </VisuallyHidden>
+      {label}
+    </label>
+  );
+}
