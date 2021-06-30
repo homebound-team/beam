23f26a7dab44030200c662b9e5373e3bcd8b95e5
Brandon <bdow@homebound.com>
2021-06-30 12:43:06 -0400

    feat: ToggleChips (#147)
    * feat: ToggleChips
    
    Expected use for ToggleChips will be used in the Filter's modal. Design had named them FilterChips, however we are naming them ambiguous to the location they were originally designed for in case we want to reuse them elsewhere.
    
    Added an option to have the label for a field be hidden.
    
    * PR Feedback

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,19 +1,23 @@
 import React, { LabelHTMLAttributes } from "react";
+import { VisuallyHidden } from "react-aria";
 import { Css } from "src/Css";
 
 interface LabelProps {
   // We don't usually have `fooProps`-style props, but this is for/from react-aria
   labelProps?: LabelHTMLAttributes<HTMLLabelElement>;
   label: string;
+  // If set, it is recommended to wrap in an element with `position: relative;` set, as the label will have an absolute position.
+  hidden?: boolean;
 }
 
 /** An internal helper component for rendering form labels. */
-export function Label({ labelProps, label, ...others }: LabelProps) {
-  return (
+export function Label({ labelProps, label, hidden, ...others }: LabelProps) {
+  const labelEl = (
     <label {...labelProps} {...others} css={Css.dib.sm.gray700.mbPx(4).$}>
       {label}
     </label>
   );
+  return hidden ? <VisuallyHidden>{labelEl}</VisuallyHidden> : labelEl;
 }
 
 /** Used for showing labels within text fields. */
---
+++ src/inputs/ToggleChipGroup.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta } from "@storybook/react";
+import { useState } from "react";
+import { ToggleChipGroup } from "src/inputs/ToggleChipGroup";
+
+export default {
+  component: ToggleChipGroup,
+  title: "Inputs/Toggle Chip Groups",
+} as Meta;
+
+export function ToggleChipGroups() {
+  const [selectedValues, setSelectedValues] = useState<string[]>(["m:2"]);
+  const options = [
+    { label: "Bahamas", value: "m:1" },
+    { label: "Southern California", value: "m:2" },
+    { label: "Northern California", value: "m:3" },
+    { label: "South Bay CA", value: "m:4" },
+    { label: "Austin", value: "m:5" },
+  ];
+  return (
+    <ToggleChipGroup
+      label="Select Markets"
+      hideLabel
+      options={options}
+      values={selectedValues}
+      onChange={setSelectedValues}
+    />
+  );
+}
