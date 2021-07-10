7f405db424d37a6aa9cde51aadea765925c5a117
Brandon Dow <bdow@homebound.com>
2021-07-09 22:26:05 -0400

    fix: Updates to the Menu and storybook
    Have 'build-storybook' includes our test assets.
    Resize our test assets to be much smaller files.
    Update ButtonMenu to only accept two placements - left or right. It will always show beneath the trigger unless the space in the viewport, in which case it will smartly flip. Updated storybook to reflect the only two placements

--- src/components/ButtonMenu.stories.tsx
+++ src/components/ButtonMenu.stories.tsx
@@ -2,12 +2,12 @@ import { action } from "@storybook/addon-actions";
 import { Meta } from "@storybook/react";
 import { ButtonMenu, MenuItem } from "src/components/ButtonMenu";
 import { Css } from "src/Css";
-import { withRouter } from "src/utils/sb";
+import { withDimensions, withRouter } from "src/utils/sb";
 
 export default {
   component: ButtonMenu,
   title: "Components/Button Menus",
-  decorators: [withRouter()],
+  decorators: [withRouter(), withDimensions()],
 } as Meta;
 
 export function MenuOpen() {
@@ -19,7 +19,7 @@ export function MenuOpen() {
 
   return (
     <div css={Css.df.flexColumn.$}>
-      <h2 css={Css.lg.$}>Default - Anchored bottom left</h2>
+      <h2 css={Css.lg.$}>Default - Anchored left</h2>
       <ButtonMenu
         trigger={{ label: "Menu trigger" }}
         items={menuItems}
@@ -28,40 +28,14 @@ export function MenuOpen() {
       />
 
       <div css={Css.mtPx(200).$}>
-        <h2 css={Css.lg.$}>Anchored to bottom right</h2>
+        <h2 css={Css.lg.$}>Anchored right</h2>
         <div css={Css.mlPx(200).$}>
           <ButtonMenu
             trigger={{ label: "Menu trigger" }}
             items={menuItems}
             persistentItems={[{ label: "Persistent Action", onClick: action("Persistent action clicked") }]}
             defaultOpen
-            placement="bottom right"
-          />
-        </div>
-      </div>
-
-      <div css={Css.mtPx(200).$}>
-        <h2 css={Css.lg.$}>Anchored to top left</h2>
-        <div css={Css.mtPx(200).$}>
-          <ButtonMenu
-            trigger={{ label: "Menu trigger" }}
-            items={menuItems}
-            persistentItems={[{ label: "Persistent Action", onClick: action("Persistent action clicked") }]}
-            defaultOpen
-            placement="top left"
-          />
-        </div>
-      </div>
-
-      <div css={Css.mtPx(20).$}>
-        <h2 css={Css.lg.$}>Anchored to top right</h2>
-        <div css={Css.mlPx(200).mtPx(200).$}>
-          <ButtonMenu
-            trigger={{ label: "Menu trigger" }}
-            items={menuItems}
-            persistentItems={[{ label: "Persistent Action", onClick: action("Persistent action clicked") }]}
-            defaultOpen
-            placement="top right"
+            placement="right"
           />
         </div>
       </div>
