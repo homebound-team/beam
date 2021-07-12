c770147085e4ebb3fd2fe012160da3e2f26340c1
Stephen Haberman <shaberman@homebound.com>
2021-07-12 13:14:26 -0500

    Update snapshots.

--- src/components/Icon.test.tsx
+++ src/components/Icon.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@homebound/rtl-utils";
-import { Icon } from "src";
+import { Icon } from "src/components/Icon";
 
 describe("Icon", () => {
   it("renders without crashing", async () => {
@@ -11,19 +11,23 @@ describe("Icon", () => {
 
       <body>
         <div>
-          <svg
-            aria-hidden="true"
-            class="emotion-0"
-            data-icon="x"
-            height="24"
-            viewBox="0 0 24 24"
-            width="24"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            data-overlay-container="true"
           >
-            <path
-              d="M16.192 6.34399L11.949 10.586L7.70697 6.34399L6.29297 7.75799L10.535 12L6.29297 16.242L7.70697 17.656L11.949 13.414L16.192 17.656L17.606 16.242L13.364 12L17.606 7.75799L16.192 6.34399Z"
-            />
-          </svg>
+            <svg
+              aria-hidden="true"
+              class="emotion-0"
+              data-icon="x"
+              height="24"
+              viewBox="0 0 24 24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.192 6.34399L11.949 10.586L7.70697 6.34399L6.29297 7.75799L10.535 12L6.29297 16.242L7.70697 17.656L11.949 13.414L16.192 17.656L17.606 16.242L13.364 12L17.606 7.75799L16.192 6.34399Z"
+              />
+            </svg>
+          </div>
         </div>
       </body>
     `);
