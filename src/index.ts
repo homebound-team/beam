b85f11886cd6f556c1ea907b422e4056e9e92c91
Stephen Haberman <stephen.haberman@gmail.com>
2021-07-12 09:30:57 -0500

    feat: Sorting refactor + ability to initially sort (#168)
    * feat: Refactor GridTable sorting API, ability to initial sort.
    
    * Rename/fix stories.
    
    * Add comments on SortHeader.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./forms";
 export * from "./hooks";
 export * from "./inputs";
 export * from "./interfaces";
+export * from "./utils/defaultTestId";
 export { withBeamRTL } from "./utils/rtl";
 export { withBeamDecorator } from "./utils/sb";
 export * from "./utils/useTestIds";
