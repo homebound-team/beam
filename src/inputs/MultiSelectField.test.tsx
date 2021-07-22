54f2b8d7f811bb7f234d7a67562e9a95e6672fb3
Stephen Haberman <stephen.haberman@gmail.com>
2021-07-22 11:24:05 -0500

    feat: Add nothingSelectedText to customize All behavior. (#186)

--- src/inputs/MultiSelectField.test.tsx
+++ src/inputs/MultiSelectField.test.tsx
@@ -1,6 +1,7 @@
-import { click, input, render } from "@homebound/rtl-utils";
+import { click, input, render, RenderResult } from "@homebound/rtl-utils";
 import { useState } from "react";
-import { MultiSelectField, MultiSelectFieldProps, Value } from "src/inputs";
+import { MultiSelectField, MultiSelectFieldProps } from "src/inputs";
+import { HasIdAndName, Optional } from "src/types";
 
 const options = [
   { id: "1", name: "One" },
@@ -12,31 +13,48 @@ describe("MultiSelectFieldTest", () => {
   const onSelect = jest.fn();
 
   it("can set a value", async () => {
-    // Given a MultiSelectField
-    const { getByRole } = await render(
-      <TestMultiSelectField
-        label="Age"
-        values={["1"] as string[]}
-        options={options}
-        getOptionLabel={(o) => o.name}
-        getOptionValue={(o) => o.id}
-      />,
-    );
+    // Given a MultiSelectField with 1 selected value
+    const r = await render(<TestMultiSelectField values={["1"] as string[]} options={options} />);
     // That initially has "One" selected
-    const text = getByRole("combobox");
-    expect(text).toHaveValue("One");
+    expect(r.getByRole("combobox")).toHaveValue("One");
     // When we select the 3rd option
-    text.focus();
-    input(text, "");
-    click(getByRole("option", { name: "Three" }));
+    selectOption(r, "Three");
     // Then onSelect was called
     expect(onSelect).toHaveBeenCalledWith(["1", "3"]);
   });
 
-  function TestMultiSelectField<O, V extends Value>(props: Omit<MultiSelectFieldProps<O, V>, "onSelect">): JSX.Element {
-    const [selected, setSelected] = useState<V[]>(props.values);
+  it("has an empty text box not set", async () => {
+    // Given a MultiSelectField with no selected values
+    const { getByRole } = await render(<TestMultiSelectField values={[]} options={options} />);
+    // That initially has "One" selected
+    const text = getByRole("combobox");
+    expect(text).toHaveValue("");
+  });
+
+  it("can have custom an empty text", async () => {
+    // Given a MultiSelectField with no selected values
+    const r = await render(<TestMultiSelectField values={[]} options={options} nothingSelectedText="All" />);
+    // That initially has "One" selected
+    const text = r.getByRole("combobox");
+    expect(text).toHaveValue("All");
+    // And when we select the first option
+    selectOption(r, "One");
+    // Then it changes to that one
+    expect(text).toHaveValue("One");
+  });
+
+  function TestMultiSelectField(
+    props: Optional<
+      MultiSelectFieldProps<HasIdAndName<string>, string>,
+      "label" | "onSelect" | "getOptionLabel" | "getOptionValue"
+    >,
+  ): JSX.Element {
+    const [selected, setSelected] = useState(props.values);
     return (
-      <MultiSelectField<O, V>
+      <MultiSelectField
+        label="Age"
+        getOptionLabel={(o) => o.name}
+        getOptionValue={(o) => o.id}
         {...props}
         values={selected}
         onSelect={(values) => {
@@ -47,3 +65,10 @@ describe("MultiSelectFieldTest", () => {
     );
   }
 });
+
+function selectOption(r: RenderResult, name: string): void {
+  const text = r.getByRole("combobox");
+  text.focus();
+  input(text, "");
+  click(r.getByRole("option", { name }));
+}
