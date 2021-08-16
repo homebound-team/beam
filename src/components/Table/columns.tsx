66af5776e9ee2e0d6e65adae49e62b60b3fc9b97
Brandon <bdow@homebound.com>
2021-08-16 13:00:40 -0400

    fix: Support both GridColumn generics in column helpers (#236)

--- src/components/Table/columns.tsx
+++ src/components/Table/columns.tsx
@@ -1,22 +1,22 @@
 import { GridColumn, Kinded } from "src/components/Table/GridTable";
 
 /** Provides default styling for a GridColumn representing a Date. */
-export function column<T extends Kinded>(columnDef: GridColumn<T>): GridColumn<T> {
+export function column<T extends Kinded, S = {}>(columnDef: GridColumn<T, S>): GridColumn<T, S> {
   return { ...columnDef };
 }
 
 /** Provides default styling for a GridColumn representing a Date. */
-export function dateColumn<T extends Kinded>(columnDef: GridColumn<T>): GridColumn<T> {
+export function dateColumn<T extends Kinded, S = {}>(columnDef: GridColumn<T, S>): GridColumn<T, S> {
   return { ...columnDef, align: "left" };
 }
 
 /**
  * Provides default styling for a GridColumn representing a Numeric value (Price, percentage, PO #, etc.). */
-export function numericColumn<T extends Kinded>(columnDef: GridColumn<T>): GridColumn<T> {
+export function numericColumn<T extends Kinded, S = {}>(columnDef: GridColumn<T, S>): GridColumn<T, S> {
   return { ...columnDef, align: "right" };
 }
 
 /** Provides default styling for a GridColumn representing an Action. */
-export function actionColumn<T extends Kinded>(columnDef: GridColumn<T>): GridColumn<T> {
+export function actionColumn<T extends Kinded, S = {}>(columnDef: GridColumn<T, S>): GridColumn<T, S> {
   return { ...columnDef, align: "center" };
 }
