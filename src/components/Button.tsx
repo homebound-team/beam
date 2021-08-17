ed8fb6139a5c74a31f7be6f949a64cdb155d89d6
Brandon <bdow@homebound.com>
2021-08-17 12:04:05 -0400

    feat: CH-6240: Update Beam Button to have feature parity with BP buttons. (#237)
    * feat: CH-6240: Update Beam Button to have feature parity with BP buttons.
    
    Allow Beam buttons to render as links, both absolute and relative (using react-router).
    Allow Beam buttons to render tooltips for non-disabled buttons
    
    * Support links in IconButton

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,62 +1,94 @@
 import { AriaButtonProps } from "@react-types/button";
 import { ReactNode, RefObject, useMemo, useRef } from "react";
 import { useButton, useFocusRing, useHover } from "react-aria";
-import { Icon, IconProps } from "src";
+import { Link } from "react-router-dom";
+import { Icon, IconProps, navLink } from "src";
 import { Tooltip } from "src/components/Tooltip";
 import { Css } from "src/Css";
 import { BeamButtonProps, BeamFocusableProps } from "src/interfaces";
+import { isAbsoluteUrl, noop } from "src/utils";
 
 export interface ButtonProps extends BeamButtonProps, BeamFocusableProps {
   label: string;
   variant?: ButtonVariant;
   size?: ButtonSize;
   icon?: IconProps["icon"];
+  /** Displays contents after the Button's label. Will be ignored for Buttons rendered as a link with an absolute URL */
   endAdornment?: ReactNode;
   /** HTML attributes to apply to the button element when it is being used to trigger a menu. */
   menuTriggerProps?: AriaButtonProps;
-  buttonRef?: RefObject<HTMLButtonElement>;
+  buttonRef?: RefObject<HTMLElement>;
 }
 
 export function Button(props: ButtonProps) {
-  const { onClick: onPress, disabled, endAdornment, menuTriggerProps, ...otherProps } = props;
+  const { onClick: onPress, disabled, endAdornment, menuTriggerProps, tooltip, ...otherProps } = props;
   const isDisabled = !!disabled;
   const ariaProps = { onPress, isDisabled, ...otherProps, ...menuTriggerProps };
   const { label, icon, variant = "primary", size = "sm", buttonRef } = ariaProps;
   const ref = buttonRef || useRef(null);
-  const { buttonProps, isPressed } = useButton(ariaProps, ref as RefObject<HTMLButtonElement>);
+  const { buttonProps, isPressed } = useButton(
+    {
+      ...ariaProps,
+      onPress: typeof onPress === "string" ? noop : onPress,
+      elementType: typeof onPress === "string" ? "a" : "button",
+    },
+    ref as RefObject<HTMLElement>,
+  );
   const { isFocusVisible, focusProps } = useFocusRing(ariaProps);
   const { hoverProps, isHovered } = useHover(ariaProps);
-  const { baseStyles, hoverStyles, disabledStyles, pressedStyles } = useMemo(() => getButtonStyles(variant, size), [
-    variant,
-    size,
-  ]);
+  const { baseStyles, hoverStyles, disabledStyles, pressedStyles } = useMemo(
+    () => getButtonStyles(variant, size),
+    [variant, size],
+  );
   const focusRingStyles = useMemo(() => (variant === "danger" ? Css.bshDanger.$ : Css.bshFocus.$), [variant]);
 
-  const button = (
-    <button
-      ref={ref}
-      {...buttonProps}
-      {...focusProps}
-      {...hoverProps}
-      css={{
-        ...Css.buttonBase.$,
-        ...baseStyles,
-        ...(isHovered && !isPressed ? hoverStyles : {}),
-        ...(isPressed ? pressedStyles : {}),
-        ...(isDisabled ? { ...disabledStyles, ...Css.cursorNotAllowed.$ } : {}),
-        ...(isFocusVisible ? focusRingStyles : {}),
-      }}
-    >
+  const buttonContent = (
+    <>
       {icon && <Icon xss={iconStyles[size]} icon={icon} />}
       {label}
-      {endAdornment && <span css={Css.ml1.$}>{endAdornment}</span>}
-    </button>
+      {/* Do not apply endAdornment for links to Absolute URLs. Component will apply the 'linkExternal' icon as an endAdornment by default */}
+      {!(typeof onPress === "string" && isAbsoluteUrl(onPress)) && endAdornment && (
+        <span css={Css.ml1.$}>{endAdornment}</span>
+      )}
+    </>
   );
+  const buttonAttrs = {
+    ref: ref as any,
+    ...buttonProps,
+    ...focusProps,
+    ...hoverProps,
+    css: {
+      ...Css.buttonBase.$,
+      ...baseStyles,
+      ...(isHovered && !isPressed ? hoverStyles : {}),
+      ...(isPressed ? pressedStyles : {}),
+      ...(isDisabled ? { ...disabledStyles, ...Css.cursorNotAllowed.$ } : {}),
+      ...(isFocusVisible ? focusRingStyles : {}),
+    },
+  };
 
-  // If we're disabled b/c of a non-boolean ReactNode, show it in a tooltip
-  if (isDisabled && typeof disabled !== "boolean") {
+  const button =
+    typeof onPress === "string" ? (
+      isAbsoluteUrl(onPress) ? (
+        <a {...buttonAttrs} href={onPress} className={navLink} target="_blank" rel="noreferrer noopener">
+          {buttonContent}
+          <span css={Css.ml1.$}>
+            <Icon icon="linkExternal" />
+          </span>
+        </a>
+      ) : (
+        <Link {...buttonAttrs} to={onPress} className={navLink}>
+          {buttonContent}
+        </Link>
+      )
+    ) : (
+      <button {...buttonAttrs}>{buttonContent}</button>
+    );
+
+  // If we're disabled b/c of a non-boolean ReactNode, or the caller specified tooltip text, then show it in a tooltip
+  if ((isDisabled && typeof disabled !== "boolean") || tooltip) {
     return (
-      <Tooltip title={disabled} delay={100}>
+      <Tooltip title={isDisabled && typeof disabled !== "boolean" ? disabled : tooltip} placement="top">
         {button}
       </Tooltip>
     );
@@ -72,38 +104,36 @@ function getButtonStyles(variant: ButtonVariant, size: ButtonSize) {
   };
 }
 
-const variantStyles: Record<
-  ButtonVariant,
-  { baseStyles: {}; hoverStyles: {}; disabledStyles: {}; pressedStyles: {} }
-> = {
-  primary: {
-    baseStyles: Css.bgLightBlue700.white.$,
-    hoverStyles: Css.bgLightBlue900.$,
-    pressedStyles: Css.bgLightBlue500.$,
-    disabledStyles: Css.bgLightBlue200.$,
-  },
+const variantStyles: Record<ButtonVariant, { baseStyles: {}; hoverStyles: {}; disabledStyles: {}; pressedStyles: {} }> =
+  {
+    primary: {
+      baseStyles: Css.bgLightBlue700.white.$,
+      hoverStyles: Css.bgLightBlue900.$,
+      pressedStyles: Css.bgLightBlue500.$,
+      disabledStyles: Css.bgLightBlue200.$,
+    },
 
-  secondary: {
-    baseStyles: Css.bgWhite.bGray300.bw1.ba.gray800.$,
-    hoverStyles: Css.bgGray100.$,
-    pressedStyles: Css.bgGray200.$,
-    disabledStyles: Css.bgWhite.gray400.$,
-  },
+    secondary: {
+      baseStyles: Css.bgWhite.bGray300.bw1.ba.gray800.$,
+      hoverStyles: Css.bgGray100.$,
+      pressedStyles: Css.bgGray200.$,
+      disabledStyles: Css.bgWhite.gray400.$,
+    },
 
-  tertiary: {
-    baseStyles: Css.bgTransparent.lightBlue700.$,
-    hoverStyles: Css.bgGray100.$,
-    pressedStyles: Css.lightBlue900.$,
-    disabledStyles: Css.gray400.$,
-  },
+    tertiary: {
+      baseStyles: Css.bgTransparent.lightBlue700.$,
+      hoverStyles: Css.bgGray100.$,
+      pressedStyles: Css.lightBlue900.$,
+      disabledStyles: Css.gray400.$,
+    },
 
-  danger: {
-    baseStyles: Css.bgRed900.white.$,
-    hoverStyles: Css.bgRed500.$,
-    pressedStyles: Css.bgRed900.$,
-    disabledStyles: Css.bgRed200.$,
-  },
-};
+    danger: {
+      baseStyles: Css.bgRed900.white.$,
+      hoverStyles: Css.bgRed500.$,
+      pressedStyles: Css.bgRed900.$,
+      disabledStyles: Css.bgRed200.$,
+    },
+  };
 
 const sizeStyles: Record<ButtonSize, {}> = {
   sm: Css.hPx(32).pxPx(12).$,
