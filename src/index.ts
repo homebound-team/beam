89c4ebb202b566bb695c70c168960a5295142c08
Brandon <bdow@homebound.com>
2021-08-17 16:52:37 -0400

    fix: Remove dependency of rtl-util from build (#240)
    Bumping rtl-utils to 2.48.0.
    Remove exporting of Beam's RTL and Storybook helpers, which have a direct dependency on @homebound/rtl-utils. This was causing rtl-util to be bundled with our production builds in frontend applications.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,4 @@ export * from "./hooks";
 export * from "./inputs";
 export * from "./interfaces";
 export * from "./utils/defaultTestId";
-export { withBeamRTL } from "./utils/rtl";
-export { withBeamDecorator } from "./utils/sb";
 export * from "./utils/useTestIds";
