483fb0375261e85c0fe9db1863b6cdd8edd79c19
Kimiko Kano <ksk5280@users.noreply.github.com>
2021-09-07 10:47:10 -0600

    fix: pass disabled prop to select element (#282)

--- src/inputs/SelectField.mock.tsx
+++ src/inputs/SelectField.mock.tsx
@@ -14,6 +14,7 @@ export function SelectField<T extends object, V extends Key>(props: SelectFieldP
     errorMsg,
     onBlur,
     onFocus,
+    disabled,
   } = props;
   const tid = useTestIds(props, "select");
 
@@ -37,7 +38,7 @@ export function SelectField<T extends object, V extends Key>(props: SelectFieldP
         if (!readOnly && onBlur) onBlur();
       }}
       // Read Only does not apply to `select` fields, instead we'll add in disabled for tests to verify.
-      disabled={readOnly}
+      disabled={disabled || readOnly}
       data-error={!!errorMsg}
       data-errormsg={errorMsg}
       data-readonly={readOnly}
