1fbf0cdeb480f8135e31f1ba91c16f38364f4fa9
Stephen Haberman <stephen.haberman@gmail.com>
2021-09-10 08:59:15 -0500

    feat: Add FormHeading proof-of-concept. (#284)
    * feat: Add FormHeading proof-of-concept.
    
    * Use h3.

---
+++ src/forms/FormHeading.tsx
@@ -0,0 +1,27 @@
+import { Css, Margin, Xss } from "src/Css";
+
+export interface FormHeadingProps {
+  title: string;
+  xss?: Xss<Margin>;
+  // This is passed automatically by FormLines
+  isFirst?: boolean;
+}
+
+export function FormHeading(props: FormHeadingProps) {
+  const { title, xss, isFirst = false } = props;
+  return (
+    <h3
+      css={{
+        ...Css.baseEm.$,
+        // Add space before the heading, but only if it's not first.
+        ...(!isFirst && Css.mt4.$),
+        ...xss,
+      }}
+    >
+      {title}
+    </h3>
+  );
+}
+
+// https://github.com/gaearon/react-hot-loader/issues/304#issuecomment-456569720
+FormHeading.isFormHeading = true;
