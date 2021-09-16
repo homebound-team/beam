b70e2ef2034a4f6c09253c5945918c062b003e29
Brandon <bdow@homebound.com>
2021-09-16 19:29:55 -0400

    fix: Handle virtualized components within nested scroll layout components (#301)

--- src/components/Layout/ScrollableContent.tsx
+++ src/components/Layout/ScrollableContent.tsx
@@ -1,9 +1,27 @@
 import { ReactNode, ReactPortal } from "react";
 import { createPortal } from "react-dom";
+import { FullBleed } from "src/components/Layout/FullBleed";
 import { useScrollableParent } from "src/components/Layout/ScrollableParent";
+import { Css } from "src/Css";
 
 /** Helper component for placing scrollable content within a `NestedScrollProvider`. */
-export function ScrollableContent({ children }: { children: ReactNode }): ReactPortal {
-  const { scrollableEl } = useScrollableParent();
-  return createPortal(children, scrollableEl);
+export function ScrollableContent({
+  children,
+  virtualized = false,
+}: {
+  children: ReactNode;
+  virtualized?: boolean;
+}): ReactPortal {
+  const { scrollableEl, pl } = useScrollableParent();
+  return createPortal(
+    !virtualized ? (
+      children
+    ) : (
+      // To prevent Virtuoso's scrollbar from being set in based on the Layout's padding, we will use the FullBleed component w/o padding to push it back over
+      <FullBleed omitPadding>
+        <div css={Css.h100.pl(pl).$}>{children}</div>
+      </FullBleed>
+    ),
+    scrollableEl,
+  );
 }
