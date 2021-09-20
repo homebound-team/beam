876eafeb73b8d8568149d2edf4dd4a14fa524252
Brandon Dow <bdow@homebound.com>
2021-09-20 10:44:10 -0400

    fix: Provide escape hatch for rendering ScrollableContent children not within ScrollableParent context

---
+++ src/components/Layout/ScrollableContent.test.tsx
@@ -0,0 +1,28 @@
+import { ScrollableContent, ScrollableParent } from "src/components/Layout";
+import { render } from "src/utils/rtl";
+
+describe("ScrollableContent", () => {
+  it("renders ScrollableContent children in portal", async () => {
+    const r = await render(
+      <ScrollableParent>
+        <ScrollableContent>
+          <div data-testid="pageContent">Hello World!</div>
+        </ScrollableContent>
+      </ScrollableParent>,
+    );
+
+    expect(r.pageContent()).toBeTruthy();
+  });
+
+  it("renders ScrollableContent children inline if not wrapped in ScrollableParent", async () => {
+    const r = await render(
+      <div>
+        <ScrollableContent>
+          <div data-testid="pageContent">Hello World!</div>
+        </ScrollableContent>
+      </div>,
+    );
+
+    expect(r.pageContent()).toBeTruthy();
+  });
+});
