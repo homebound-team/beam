8ed9e6a3f0cdad2c23d7b137a2d5ada6ac4deb98
Brandon Dow <bdow@homebound.com>
2021-10-14 10:54:18 -0400

    Support size property for DateField to allow DateFilter to adjust size properly when shown as an 'in page' filter

--- src/components/Filters/DateFilter.stories.tsx
+++ src/components/Filters/DateFilter.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta } from "@storybook/react";
 import { taskDueFilter } from "src/components/Filters/testDomain";
 import { Filters } from "src/components/index";
+import { Css } from "src/Css";
 import { jan2 } from "src/forms/formStateDomain";
 
 export default {
@@ -11,7 +12,8 @@ export default {
 
 export function DateFilterInPage() {
   const filter = taskDueFilter("date");
-  return filter.render({ op: "BEFORE", date: jan2 }, () => {}, {}, false, false);
+  // Mimics the container of the "in page" filters to demonstrate the field can shrink in size.
+  return <div css={Css.df.aic.$}>{filter.render({ op: "BEFORE", date: jan2 }, () => {}, {}, false, false)}</div>;
 }
 
 export function DateFilterInModal() {
