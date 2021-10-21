60042fde3e38efd902ec7d84a9c13f11ee0caaf1
Brandon Dow <bdow@homebound.com>
2021-10-21 15:39:15 -0400

    fix: ButtonMenu items should update when 'items' prop changes
    Adds testid support for ButtonMenu

--- src/components/ButtonMenu.stories.tsx
+++ src/components/ButtonMenu.stories.tsx
@@ -1,5 +1,7 @@
 import { action } from "@storybook/addon-actions";
 import { Meta } from "@storybook/react";
+import { useState } from "react";
+import { Button } from "src/components/Button";
 import { ButtonMenu, MenuItem } from "src/components/ButtonMenu";
 import { Css } from "src/Css";
 import { withDimensions, withRouter } from "src/utils/sb";
@@ -44,18 +46,37 @@ export function MenuOpen() {
 }
 
 export function InteractiveMenu() {
-  const menuItems: MenuItem[] = [
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([
     { label: "Page action", onClick: action("Test item clicked") },
     { label: "Internal Link", onClick: "/fakeRoute" },
     { label: "External Link - Homebound.com", onClick: "https://www.homebound.com" },
-  ];
+  ]);
 
   return (
-    <ButtonMenu
-      trigger={{ label: "Menu trigger" }}
-      items={menuItems}
-      persistentItems={[{ label: "Persistent Action", onClick: action("Persistent action clicked") }]}
-    />
+    <>
+      <div css={Css.df.gap1.mb2.$}>
+        <Button
+          label="Toggle Disable of first item"
+          onClick={() =>
+            setMenuItems(([pageAction, ...others]) => [{ ...pageAction, disabled: !pageAction.disabled }, ...others])
+          }
+        />
+        <Button
+          label="Add New Menu Item"
+          onClick={() =>
+            setMenuItems((prevItems) => [
+              ...prevItems,
+              { label: `Item ${prevItems.length + 1}`, onClick: action(`Item ${prevItems.length + 1}`) },
+            ])
+          }
+        />
+      </div>
+      <ButtonMenu
+        trigger={{ label: "Menu trigger" }}
+        items={menuItems}
+        persistentItems={[{ label: "Persistent Action", onClick: action("Persistent action clicked") }]}
+      />
+    </>
   );
 }
 
--- src/components/ButtonMenu.tsx
+++ src/components/ButtonMenu.tsx
@@ -1,5 +1,6 @@
 import type { Placement } from "@react-types/overlays";
-import { useRef } from "react";
+import { camelCase } from "change-case";
+import { useEffect, useRef } from "react";
 import { DismissButton, useMenuTrigger, useOverlayPosition } from "react-aria";
 import { Item, Section, useMenuTriggerState, useTreeData } from "react-stately";
 import { Button, ButtonProps } from "src/components/Button";
@@ -9,6 +10,8 @@ import { Popover } from "src/components/internal";
 import { Menu } from "src/components/internal/Menu";
 import { Css } from "src/Css";
 import { Callback } from "src/types";
+import { useTestIds } from "src/utils";
+import { defaultTestId } from "src/utils/defaultTestId";
 
 interface TextButtonTriggerProps extends Pick<ButtonProps, "label" | "variant" | "size" | "icon"> {}
 interface IconButtonTriggerProps extends Pick<IconButtonProps, "icon" | "color"> {}
@@ -24,7 +27,8 @@ interface ButtonMenuProps {
   disabled?: boolean;
 }
 
-export function ButtonMenu({ trigger, items, placement, persistentItems, defaultOpen, disabled }: ButtonMenuProps) {
+export function ButtonMenu(props: ButtonMenuProps) {
+  const { trigger, items, placement, persistentItems, defaultOpen, disabled } = props;
   const state = useMenuTriggerState({ isOpen: defaultOpen });
   const buttonRef = useRef<HTMLButtonElement>(null);
   const popoverRef = useRef(null);
@@ -37,6 +41,7 @@ export function ButtonMenu({ trigger, items, placement, persistentItems, default
     onClose: state.close,
     placement: (placement ? `bottom ${placement}` : "bottom left") as Placement,
   });
+  const tid = useTestIds(props, isTextButton(trigger) ? defaultTestId(trigger.label) : trigger.icon);
 
   // Build out the Menu's Tree data to include the Persistent Action, if any. This is a collection of Nodes that is used
   // by React-Aria to keep track of item states such as focus, and provide hooks for calling those actions.
@@ -44,8 +49,19 @@ export function ButtonMenu({ trigger, items, placement, persistentItems, default
     initialItems: [items, persistentItems ? persistentItems : []].flatMap((i, idx) =>
       i.length > 0 ? ([{ label: idx === 0 ? "items" : "persistent", items: i }] as MenuSection[]) : ([] as MenuItem[]),
     ),
-    getKey: (item) => item.label.replace(/\"/g, ""),
+    getKey: (item) => camelCase(item.label),
+    getChildren: (item) => (item as MenuSection).items ?? [],
   });
+
+  // Bulk updates of MenuItems below. If we find this to be of sluggish performance, then we can change to be more surgical in our updating.
+  // If our list of items change, update the "items" menu section. (key is based on label in `getKey` above)
+  useEffect(() => tree.update("items", { label: "items", items } as MenuSection), [items]);
+
+  // NOTE: Not updating persistentItems at the moment as there seems to be a bug with this. Only updates one set of items at a time, will follow up later.
+  // If our list of persistentItems change, update the "persistent" menu section.
+  // useEffect(() => {
+  //   tree.update("persistent", { label: "persistent", items: persistentItems || [] } as MenuSection);
+  // }, [persistentItems]);
 
   return (
     <div css={Css.relative.dib.$}>
@@ -57,9 +73,10 @@ export function ButtonMenu({ trigger, items, placement, persistentItems, default
           buttonRef={buttonRef}
           endAdornment={<Icon icon={state.isOpen ? "chevronUp" : "chevronDown"} />}
           disabled={disabled}
+          {...tid}
         />
       ) : (
-        <IconButton {...trigger} menuTriggerProps={menuTriggerProps} buttonRef={buttonRef} />
+        <IconButton {...trigger} menuTriggerProps={menuTriggerProps} buttonRef={buttonRef} {...tid} />
       )}
       {state.isOpen && (
         <Popover
@@ -69,7 +86,7 @@ export function ButtonMenu({ trigger, items, placement, persistentItems, default
           onClose={() => state.close()}
           isOpen={state.isOpen}
         >
-          <Menu ariaMenuProps={ariaMenuProps} items={tree.items} onClose={() => state.close()}>
+          <Menu ariaMenuProps={ariaMenuProps} items={tree.items} onClose={() => state.close()} {...tid}>
             {(s) => (
               <Section key={s.label.replace(/\"/g, "")} title={s.label} items={s.items}>
                 {(item) => <Item key={item.label.replace(/\"/g, "")}>{item.label}</Item>}
