74822fa44d64e0a327d00b64614b4b88ec08c9ff
Brandon Dow <bdow@homebound.com>
2022-02-09 15:15:05 -0500

    PR Feedback - add spacing

--- src/components/Table/useSortState.test.ts
+++ src/components/Table/useSortState.test.ts
@@ -5,14 +5,17 @@ describe("useSortState", () => {
   it("can derive the next sort state when current state is undefined", () => {
     expect(deriveSortState(undefined, 2, undefined)).toEqual([2, ASC]);
   });
+
   it("can derive the next sort state when clicking on a new column", () => {
     expect(deriveSortState([1, ASC], 2, undefined)).toEqual([2, ASC]);
   });
+
   it("can derive the next sort state when clicking on a currently ascending column", () => {
     expect(deriveSortState([1, ASC], 1, undefined)).toEqual([1, DESC]);
     // With `initialSortState` the same as the currentSortState Ascending
     expect(deriveSortState([1, ASC], 1, [1, ASC])).toEqual([1, DESC]);
   });
+
   it("can derive the next sort state when clicking on a currently descending column", () => {
     // With `initialSortState` defined
     expect(deriveSortState([1, DESC], 1, [2, ASC])).toEqual([2, ASC]);
