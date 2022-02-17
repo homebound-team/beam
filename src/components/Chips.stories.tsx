91ec69aff3ce55c6758c936f8705112b0c449914
Brandon Dow <bdow@homebound.com>
2022-02-17 09:53:29 -0500

    fix: Prevent chips from stretching to fill container

--- src/components/Chips.stories.tsx
+++ src/components/Chips.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta } from "@storybook/react";
 import { Chips } from "src/components/Chips";
+import { PresentationProvider } from "src/components/PresentationContext";
 import { Css } from "src/Css";
 
 export default {
@@ -9,8 +10,23 @@ export default {
 
 export function DefaultChips() {
   return (
-    <div css={Css.wPx(300).ba.$}>
-      <Chips values={["First Last", "Second Last", "Third Last", "Fourth Last"]} />
-    </div>
+    <>
+      <h1>Chips wrap</h1>
+      <div css={Css.wPx(300).ba.$}>
+        <Chips values={["First Last", "Second Last", "Third Last", "Fourth Last"]} />
+      </div>
+
+      <h1 css={Css.mt3.$}>Chips truncate</h1>
+      <div css={Css.wPx(300).ba.$}>
+        <PresentationProvider wrap={false}>
+          <Chips values={["First Last", "Second Last", "Third Last", "Fourth Last"]} />
+        </PresentationProvider>
+      </div>
+
+      <h1 css={Css.mt3.$}>Chips do not stretch to fill container</h1>
+      <div css={Css.df.wPx(500).hPx(100).ba.$}>
+        <Chips values={["First Last", "Second Last", "Third Last", "Fourth Last"]} />
+      </div>
+    </>
   );
 }
