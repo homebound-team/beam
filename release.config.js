cd40df3d1e255f8931af0059342414e524c41edb
Ben Limmer <ben@benlimmer.com>
2022-03-02 15:32:48 -0700

    chore: SC-13496 update NPM contexts (#460)

--- release.config.js
+++ release.config.js
@@ -3,6 +3,12 @@ module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     ["@semantic-release/release-notes-generator", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "yarn build",
+      },
+    ],
     "@semantic-release/npm",
     "@semantic-release/github",
     "@semantic-release/git",
