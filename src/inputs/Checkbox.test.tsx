01b330722e24f97470f7c5514912baf44be7492d
Stephen Haberman <shaberman@homebound.com>
2022-03-14 11:26:37 -0500

    Add test that indeterminate becomes true.

--- src/inputs/Checkbox.test.tsx
+++ src/inputs/Checkbox.test.tsx
@@ -1,6 +1,6 @@
 import { Checkbox } from "src/inputs/Checkbox";
 import { noop } from "src/utils";
-import { render } from "src/utils/rtl";
+import { click, render } from "src/utils/rtl";
 
 describe("Checkbox", () => {
   it("can set default test ids", async () => {
@@ -11,5 +11,15 @@ describe("Checkbox", () => {
   it("can set explicit test ids", async () => {
     const r = await render(<Checkbox label="Test Label" selected={false} onChange={noop} data-testid="customId" />);
     expect(r.customId()).toBeTruthy();
+  });
+
+  it("treats indeterminate as false", async () => {
+    // Given an indeterminate checkbox
+    const onChange = jest.fn();
+    const r = await render(<Checkbox label="Test" selected="indeterminate" onChange={onChange} />);
+    // When the user clicks it
+    click(r.test);
+    // Then it becomes true
+    expect(onChange).toHaveBeenCalledWith(true);
   });
 });
