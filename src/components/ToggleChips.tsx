7a978e10124191044c7adcff5aa9bd63dec5a6e6
Brandon Dow <bdow@homebound.com>
2022-05-20 12:25:43 -0400

    fix: Support testids on ToggleChips. Fix xss support and use CSS gap for spacing chips

--- src/components/ToggleChips.tsx
+++ src/components/ToggleChips.tsx
@@ -15,11 +15,11 @@ export interface ToggleChipsProps<T, X> {
 /** Renders a list of `Chip`s, with wrapping & appropriate margin between each `Chip`. */
 export function ToggleChips<T, X extends Only<ToggleChipXss, X>>(props: ToggleChipsProps<T, X>) {
   const { values, getLabel, onRemove, xss } = props;
-  const tid = useTestIds(props, "chip");
+  const tid = useTestIds(props, "toggleChips");
   return (
-    <div css={{ ...Css.df.add({ flexWrap: "wrap" }).my1.$, xss }}>
-      {values.map((value, i) => (
-        <ToggleChip key={i} text={getLabel(value)} onClick={() => onRemove(value)} xss={Css.mr1.mb1.$} {...tid} />
+    <div css={{ ...Css.df.add("flexWrap", "wrap").gap1.$, ...xss }} {...tid}>
+      {values.map((value) => (
+        <ToggleChip key={getLabel(value)} text={getLabel(value)} onClick={() => onRemove(value)} {...tid.chip} />
       ))}
     </div>
   );
