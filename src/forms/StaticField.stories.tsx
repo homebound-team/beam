0693e00bea2a2f907171aadd04e6eb600cca92b2
Brandon Dow <bdow@homebound.com>
2022-06-09 11:10:31 -0400

    fix: Update StaticField to match readOnly input field styles

--- src/forms/StaticField.stories.tsx
+++ src/forms/StaticField.stories.tsx
@@ -2,6 +2,7 @@ import { Meta } from "@storybook/react";
 import { Chips } from "src/components/Chips";
 import { FieldGroup, FormLines } from "src/forms/FormLines";
 import { StaticField as StaticFieldComponent } from "src/forms/StaticField";
+import { TextField } from "src/inputs";
 
 export default {
   component: StaticFieldComponent,
@@ -12,9 +13,10 @@ export function StaticField() {
   return (
     <FormLines>
       <StaticFieldComponent label="First" value="Bob" />
-      <FieldGroup>
+      <FieldGroup widths={["100px", "100px", "200px"]}>
         <StaticFieldComponent label="First" value="Bob" />
         <StaticFieldComponent label="First" value="Bob" />
+        <TextField label="First (read only TextField)" value="Bob" onChange={() => {}} readOnly />
       </FieldGroup>
       <StaticFieldComponent label="First">
         <Chips values={["First", "Last"]} />
--- src/forms/StaticField.tsx
+++ src/forms/StaticField.tsx
@@ -1,5 +1,6 @@
+import { useId } from "@react-aria/utils";
 import { ReactNode } from "react";
-import { Css, px } from "src/Css";
+import { Css } from "src/Css";
 import { defaultTestId } from "src/utils/defaultTestId";
 import { useTestIds } from "src/utils/useTestIds";
 
@@ -12,11 +13,13 @@ interface StaticFieldProps {
 export function StaticField(props: StaticFieldProps) {
   const { label, value, children } = props;
   const tid = useTestIds(props, defaultTestId(label));
+  const id = useId();
   return (
     <div>
-      <label css={Css.db.sm.gray700.mbPx(4).$}>{label}</label>
-      {/*Our form fields are 40px high, so match that.*/}
-      <div css={Css.sm.gray900.mh(px(40)).df.aic.$} {...tid}>
+      <label css={Css.db.sm.gray700.mbPx(4).$} htmlFor={id} {...tid.label}>
+        {label}
+      </label>
+      <div id={id} css={Css.smEm.gray900.df.aic.$} {...tid}>
         {value || children}
       </div>
     </div>
