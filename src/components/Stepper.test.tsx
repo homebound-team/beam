9e8e451dacc15abbd0b004e60015499a0db2eb3c
Brandon <bdow@homebound.com>
2022-07-25 16:55:49 -0400

    fix: Add testids to Stepper component (#607)

---
+++ src/components/Stepper.test.tsx
@@ -0,0 +1,34 @@
+import { Step, Stepper } from "src/components/Stepper";
+import { click, render } from "src/utils/rtl";
+import { zeroTo } from "src/utils/sb";
+
+describe(Stepper, () => {
+  it("renders and invokes onChange", async () => {
+    const onChange = jest.fn();
+    // Given the stepper with 3 steps. The first disabled and complete, the second incomplete, and the third in error state.
+    const steps: Step[] = zeroTo(3).map((idx) => ({
+      label: `Step ${idx === 0 ? "A" : idx === 1 ? "B" : "C"}`,
+      value: `step${idx + 1}`,
+      state: "incomplete",
+      disabled: idx === 0,
+    }));
+    // When rendered
+    const r = await render(<Stepper steps={steps} currentStep={steps[1].value} onChange={onChange} />);
+
+    // Then all three steps are displayed and have the proper `aria-current` attribute
+    expect(r.stepper_step_0()).toHaveAttribute("aria-current", "false");
+    expect(r.stepper_step_1()).toHaveAttribute("aria-current", "true");
+    expect(r.stepper_step_2()).toHaveAttribute("aria-current", "false");
+
+    // And the button elements for each step have the expected states
+    expect(r.stepper_stepButton_stepA()).toBeDisabled().toHaveTextContent("Step A");
+    expect(r.stepper_stepButton_stepB()).not.toBeDisabled().toHaveTextContent("Step B");
+    expect(r.stepper_stepButton_stepC()).not.toBeDisabled().toHaveTextContent("Step C");
+    expect(r.stepper_stepButton_stepC()).not.toBeDisabled().toHaveTextContent("Step C");
+
+    // And when clicking Step 3
+    click(r.stepper_stepButton_stepC);
+    // Then the `onChange` callback is invoked
+    expect(onChange).toBeCalledWith("step3");
+  });
+});
--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -2,6 +2,8 @@ import { useRef } from "react";
 import { useButton, useFocusRing, useHover } from "react-aria";
 import { Icon } from "src/components/Icon";
 import { Css } from "src/Css";
+import { useTestIds } from "src/utils";
+import { defaultTestId } from "src/utils/defaultTestId";
 
 export interface Step {
   label: string;
@@ -17,10 +19,12 @@ export interface StepperProps {
   onChange: (stepValue: string) => void;
 }
 
-export function Stepper({ steps, currentStep, onChange }: StepperProps) {
+export function Stepper(props: StepperProps) {
+  const { steps, currentStep, onChange } = props;
   if (steps.length === 0) {
     throw new Error("Stepper must be initialized with at least one step");
   }
+  const tid = useTestIds(props, "stepper");
 
   // calc progress based on last completed step - return -1 when no steps completed
   const lastCompletedStep = steps.map((step) => step.state).lastIndexOf("complete");
@@ -29,7 +33,7 @@ export function Stepper({ steps, currentStep, onChange }: StepperProps) {
   const gap = 8;
 
   return (
-    <nav aria-label="steps" css={Css.df.fdc.w100.$}>
+    <nav aria-label="steps" css={Css.df.fdc.w100.$} {...tid}>
       <ol css={Css.listReset.df.gapPx(gap).$}>
         {steps.map((step) => {
           const isCurrent = currentStep === step.value;
@@ -38,8 +42,9 @@ export function Stepper({ steps, currentStep, onChange }: StepperProps) {
               css={Css.df.fg1.fdc.maxwPx(maxStepWidth).mwPx(minStepWidth).$}
               key={step.label}
               aria-current={isCurrent}
+              {...tid.step}
             >
-              <StepButton {...step} onClick={() => onChange(step.value)} isCurrent={isCurrent} />
+              <StepButton {...step} onClick={() => onChange(step.value)} isCurrent={isCurrent} {...tid.stepButton} />
             </li>
           );
         })}
@@ -69,13 +74,15 @@ interface StepButtonProps extends Step {
   isCurrent: boolean;
 }
 
-function StepButton({ label, disabled, state, isCurrent, onClick }: StepButtonProps) {
+function StepButton(props: StepButtonProps) {
+  const { label, disabled, state, isCurrent, onClick } = props;
   const ariaProps = { onPress: onClick, isDisabled: disabled };
   const ref = useRef(null);
   const { buttonProps, isPressed } = useButton(ariaProps, ref);
   const { isFocusVisible, focusProps } = useFocusRing();
   const { hoverProps, isHovered } = useHover(ariaProps);
   const focusRingStyles = state === "error" ? Css.bshDanger.$ : Css.bshFocus.$;
+  const tid = useTestIds(props, "stepButton");
 
   return (
     <button
@@ -92,6 +99,7 @@ function StepButton({ label, disabled, state, isCurrent, onClick }: StepButtonPr
         ...(disabled ? Css.gray400.cursorNotAllowed.if(state === "error").red200.$ : {}),
         ...(isFocusVisible ? focusRingStyles : {}),
       }}
+      {...tid[defaultTestId(label)]}
     >
       <span css={Css.fs0.mrPx(4).$}>
         <StepIcon state={state} isHovered={isHovered} isPressed={isPressed} isCurrent={isCurrent} />
