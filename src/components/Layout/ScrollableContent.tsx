0bed6dbea23c6401dd75e01d0834a6b08a01b5ba
Brandon <bdow@homebound.com>
2022-09-01 12:04:17 -0400

    fix: Ability to omit bottom padding from ScrollableContent (#640)
    In Blueprint there are pages such as Documents and To Dos which nest their scroll handling. There are potentially better ways to achieve this, but the simplest way at the moment is using this prop.

--- src/components/Layout/ScrollableContent.tsx
+++ src/components/Layout/ScrollableContent.tsx
@@ -4,9 +4,14 @@ import { FullBleed } from "src/components/Layout/FullBleed";
 import { scrollContainerBottomPadding, useScrollableParent } from "src/components/Layout/ScrollableParent";
 import { Css } from "src/Css";
 
+interface ScrollableContentProps {
+  children: ReactNode;
+  virtualized?: boolean;
+  omitBottomPadding?: true;
+}
 /** Helper component for placing scrollable content within a `ScrollableParent`. */
-export function ScrollableContent(props: { children: ReactNode; virtualized?: boolean }): ReactPortal | JSX.Element {
-  const { children, virtualized = false } = props;
+export function ScrollableContent(props: ScrollableContentProps): ReactPortal | JSX.Element {
+  const { children, virtualized = false, omitBottomPadding } = props;
   const { scrollableEl, setPortalTick, pl } = useScrollableParent();
 
   useEffect(() => {
@@ -24,7 +29,11 @@ export function ScrollableContent(props: { children: ReactNode; virtualized?: bo
 
   return createPortal(
     !virtualized ? (
-      <div css={scrollContainerBottomPadding}>{children}</div>
+      omitBottomPadding ? (
+        children
+      ) : (
+        <div css={scrollContainerBottomPadding}>{children}</div>
+      )
     ) : (
       // To prevent Virtuoso's scrollbar from being set in based on the Layout's padding, we will use the FullBleed component w/o padding to push it back over
       <FullBleed omitPadding>
