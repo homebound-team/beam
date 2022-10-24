d5dfa112a9fb6e4600f3809713015dbfb140c5e6
Andrew Sanchez <97992415+asanchezHB@users.noreply.github.com>
2022-10-24 10:52:51 -0700

    feat: Add bxArrowIcons (#696)
    * add bxArrowIcons
    
    * feat: add bxArrowIcons
    
    * feat: update icons

--- src/components/Icon.stories.tsx
+++ src/components/Icon.stories.tsx
@@ -75,6 +75,10 @@ export const Icon = (props: IconProps) => {
     "arrowRight",
     "menuClose",
     "menuOpen",
+    "arrowFromBottom",
+    "arrowFromLeft",
+    "arrowFromRight",
+    "arrowFromTop",
   ];
   const mediaIcons: IconProps["icon"][] = ["camera", "fileBlank", "folder", "image", "file", "images", "openBook"];
   const miscIcons: IconProps["icon"][] = ["dollar", "userCircle", "calendar", "buildingHouse", "house", "bell", "customize"];
