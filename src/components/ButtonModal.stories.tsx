1c305ab4ef2bdedd55a426c5b72ef75680496f68
ivapie <ivanp@arcion.io>
2022-10-28 14:43:43 -0600

    feat: add leaf, floorPlan, chair and bolt icons, also on bannerModal component added the variant and hideEndAdorment

--- src/components/ButtonModal.stories.tsx
+++ src/components/ButtonModal.stories.tsx
@@ -44,3 +44,17 @@ export function ButtonModalInteractive() {
     />
   );
 }
+
+export function ButtonModalWithVariantAndCustomHTML() {
+  return (
+    <ButtonModal
+      storybookDefaultOpen
+      content={
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ipsum purus, bibendum sit amet vulputate eget, porta semper ligula. Donec bibendum vulputate erat, ac fringilla mi finibus nec. Donec ac dolor sed dolor porttitor blandit vel vel purus. Fusce vel malesuada ligula. Nam quis vehicula ante, eu finibus est. Proin ullamcorper fermentum orci, quis finibus massa. Nunc lobortis, massa ut rutrum ultrices, metus metus finibus ex, sit amet facilisis neque enim sed neque. Quisque accumsan metus vel maximus consequat. Suspendisse lacinia tellus a libero volutpat maximus."
+      }
+      trigger={{ label: "Button Modal trigger" }}
+      variant="text"
+      hideEndAdornment
+    />
+  );
+}
