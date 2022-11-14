5fe149bd8f814402cd55a19c0ffac42a9c562c3f
Brandon Dow <bdow@homebound.com>
2022-11-14 14:08:49 -0500

    feat: Pass 'close' method to ButtonModal content
    Allows for the ButtonModal's content to provide its own closing actions

--- src/components/ButtonModal.test.tsx
+++ src/components/ButtonModal.test.tsx
@@ -4,19 +4,31 @@ import { ButtonModal } from "./ButtonModal";
 describe("ButtonModal", () => {
   it("can open a ButtonModal", async () => {
     // Given a ButtonModal
-    const r = await render(<TestButtonModal />);
+    const r = await render(
+      <ButtonModal trigger={{ label: "Menu trigger" }} content={"Lorum Ipsum"} title={"Modal Title"} />,
+    );
     // When opening the ButtonModal
     click(r.menuTrigger);
     // Then expect Button Modal title and content to be rendered
     expect(r.popup_title).toBeTruthy();
     expect(r.popup_content).toBeTruthy();
   });
-});
 
-function TestButtonModal() {
-  return (
-    <>
-      <ButtonModal trigger={{ label: "Menu trigger" }} content={"Lorum Ipsum"} title={"Modal Title"} />
-    </>
-  );
-}
+  it("can be closed via the close prop to contextual modal", async () => {
+    // Given the ButtonModal where the content prop is a function that accepts the `close` property and passes it to the resulting component
+    function TestModal({ close }: { close: () => void }) {
+      return <button onClick={close} data-testid="close" />;
+    }
+    const r = await render(
+      <ButtonModal trigger={{ label: "Trigger" }} content={(close) => <TestModal close={close} />} />,
+    );
+
+    // When opening the modal
+    click(r.trigger);
+    // And clicking the close button within the modal content
+    click(r.close);
+
+    // Then the modal should now be closed.
+    expect(r.popup).toNotBeInTheDom();
+  });
+});
