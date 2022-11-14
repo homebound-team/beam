57e726b76df157c58f8eb1755011d2af7cbc4ed7
Brandon <bdow@homebound.com>
2022-11-14 14:15:14 -0500

    feat: Pass 'close' method to ButtonModal content (#729)
    Allows for the ButtonModal's content to provide its own closing actions

--- src/components/internal/ContextualModal.stories.tsx
+++ src/components/internal/ContextualModal.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta } from "@storybook/react";
+import { noop } from "src/utils";
 import { withDimensions } from "src/utils/sb";
 import { ContextualModal } from "./ContextualModal";
 
@@ -12,14 +13,14 @@ export default {
     design: {
       type: "figma",
       url: "https://www.figma.com/file/aWUE4pPeUTgrYZ4vaTYZQU/%E2%9C%A8Beam-Design-System?node-id=32720%3A99727",
-    }
+    },
   },
 } as Meta;
 
 export function BasicContextualModal() {
-  return <ContextualModal content="Hello" />;
+  return <ContextualModal content="Hello" close={noop} />;
 }
 
 export function BasicContextualModalWithTitle() {
-  return <ContextualModal content="Hello" title="Modal title" />;
+  return <ContextualModal content="Hello" title="Modal title" close={noop} />;
 }
--- src/components/internal/ContextualModal.tsx
+++ src/components/internal/ContextualModal.tsx
@@ -6,20 +6,21 @@ import { useTestIds } from "src/utils";
 export interface ContextualModalProps {
   content: ReactNode;
   title?: string;
+  close: () => void;
 }
 
 export function ContextualModal(props: ContextualModalProps) {
-  const { content, title } = props;
+  const { content, title, close } = props;
   const tid = useTestIds(props, "popup");
   return (
     <FocusScope restoreFocus autoFocus>
-      <div css={Css.p3.df.fdc.gap3.bgWhite.bshModal.br4.maxh("inherit").overflowAuto.$}>
+      <div css={Css.p3.df.fdc.gap3.bgWhite.bshModal.br4.maxh("inherit").overflowAuto.$} {...tid}>
         {title && (
           <div css={Css.lg.tc.$} {...tid.title}>
             {title}
           </div>
         )}
-        <div {...tid.content}>{content}</div>
+        <div {...tid.content}>{typeof content === "function" ? content(close) : content}</div>
       </div>
     </FocusScope>
   );
