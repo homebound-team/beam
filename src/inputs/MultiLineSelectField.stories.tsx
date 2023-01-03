fbad5a0c927ab0a878d926bca0d764a0dbeb7cbd
Roberth Gómez <roberthgomez@192.168.1.7>
2023-01-03 15:16:34 -0500

    add labelStyle support

--- src/inputs/MultiLineSelectField.stories.tsx
+++ src/inputs/MultiLineSelectField.stories.tsx
@@ -18,22 +18,34 @@ type TestOption = {
 };
 
 const options: TestOption[] = [
-  { id: "1", name: "Download", icon: "download" },
-  { id: "2", name: "Camera", icon: "camera" },
-  { id: "3", name: "Info Circle", icon: "infoCircle" },
-  { id: "4", name: "Calendar", icon: "calendar" },
-  { id: "5", name: "Dollar dollar bill, ya'll! ".repeat(5), icon: "dollar" },
+  { id: "sh:1", name: "Triangle" },
+  { id: "sh:2", name: "Square" },
+  { id: "sh:3", name: "Circle" },
+  { id: "sh:4", name: "Star" },
+  { id: "sh:5", name: "Rectangle".repeat(5) },
 ];
 
 export function MultiLineSelectFields() {
   const [values, setValues] = useState<Value[]>([]);
 
   return (
-    <div css={Css.df.fdc.gap5.$}>
+    <div css={Css.df.fdc.gap3.$}>
       <div css={Css.df.fdc.gap2.$}>
         <h1 css={Css.lg.$}>Regular</h1>
         <FormLines width="md">
           <MultiLineSelectField label="Favorite Icons" values={values} options={options} onSelect={(val) => setValues(val)} />
+        </FormLines>
+      </div>
+      <div css={Css.df.fdc.gap2.$}>
+        <h1 css={Css.lg.$}>With a hidden label</h1>
+        <FormLines width="md">
+          <MultiLineSelectField label="Favorite Icons" labelStyle="hidden" values={values} options={options} onSelect={(val) => setValues(val)} />
+        </FormLines>
+      </div>
+      <div css={Css.df.fdc.gap2.$}>
+        <h1 css={Css.lg.$}>With a horizontal layout</h1>
+        <FormLines width="md">
+          <MultiLineSelectField label="Favorite Icons" labelStyle="left" values={values} options={options} onSelect={(val) => setValues(val)} />
         </FormLines>
       </div>
     </div>
--- src/inputs/MultiLineSelectField.tsx
+++ src/inputs/MultiLineSelectField.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { Button } from "src/components/Button";
+import { Label } from "src/components/Label";
 import { SelectField, Value } from "src/inputs";
 import { BeamSelectFieldBaseProps } from "src/inputs/internal/SelectFieldBase";
 import { Optional } from "src/types";
-import { Css } from "..";
+import { Css, useTestIds } from "..";
 
 export interface MultiLineSelectFieldProps<O, V extends Value>
   extends Exclude<BeamSelectFieldBaseProps<O, V>, "unsetLabel"> {
@@ -23,83 +24,94 @@ export function MultiLineSelectField<O, V extends Value>(
     values,
     getOptionValue = (opt: O) => (opt as any).id,
     getOptionLabel = (opt: O) => (opt as any).name,
+    labelStyle,
     ...otherProps
   } = props;
 
+  const tid = useTestIds(props, "");
   const [isDisplayed, setIsDisplayed] = useState(true);
   const [currentOptions, setCurrentOptions] = useState(options.filter((o) => !values.includes(getOptionValue(o))));
 
   return (
-    <div css={Css.mt1.$}>
-      {values.map((value, index) => {
-        return (
-          <div css={Css.mb1.pl1.df.$} key={index}>
-            <div css={Css.truncate.w100.$}>
-              <SelectField
-                {...otherProps}
-                labelStyle="hidden"
-                data-testid="selectField"
-                value={value}
-                onSelect={() => {}}
-                options={options}
-                getOptionValue={getOptionValue}
-                getOptionLabel={getOptionLabel}
-                compact={true}
-                readOnly={true}
-              />
-            </div>
-            <Button
-              data-testid="deleteSelected"
-              variant="tertiary"
-              label={""}
-              aria-label={`Delete selected ${otherProps.label}`}
-              icon={"x"}
-              onClick={() => {
-                // Delete the selected value from the array
-                const [selectedValues, selectedOptions] = options
-                  .filter((o) => values.filter((v) => v !== value).includes(getOptionValue(o)))
-                  .reduce(
-                    (acc, o) => {
-                      acc[0].push(getOptionValue(o));
-                      acc[1].push(o);
-                      return acc;
-                    },
-                    [[] as V[], [] as O[]],
-                  );
-
-                onSelect(selectedValues, selectedOptions);
-                setCurrentOptions(options.filter((o) => !selectedOptions.includes(o)));
-              }}
-            />
-          </div>
-        );
-      })}
-      {isDisplayed && (
-        <div css={Css.mb1.$}>
-          <SelectField
-            label={otherProps.label}
-            labelStyle="hidden"
-            data-testid="selectField"
-            getOptionValue={getOptionValue}
-            getOptionLabel={getOptionLabel}
-            value={"" as string}
-            onSelect={(value) => {
-              onSelect([...values, value], options);
-              setCurrentOptions(currentOptions.filter((o) => getOptionValue(o) !== value));
-              setIsDisplayed(false);
-            }}
-            options={currentOptions}
-            disabled={otherProps.disabled}
-          />
+    <div css={Css.mt1.if(labelStyle === "left").df.$}>
+      {labelStyle !== "hidden" && (
+        <div css={Css.if(labelStyle === "left").w50.$}>
+          <Label {...tid.label} label={props.label} />
         </div>
       )}
-      <Button
-        data-testid="addAnother"
-        label={`Add Another ${otherProps.label}`}
-        variant="tertiary"
-        onClick={() => setIsDisplayed(true)}
-        disabled={isDisplayed || currentOptions.length === 0}
-      />
+      <div css={Css.if(labelStyle === "left").w50.$}>
+        {values.map((value, index) => {
+          return (
+            <div css={Css.mb1.pl1.df.$} key={index}>
+              <div css={Css.truncate.w100.$}>
+                <SelectField
+                  {...otherProps}
+                  {...tid.selectField}
+                  labelStyle="hidden"
+                  value={value}
+                  onSelect={() => {}}
+                  options={options}
+                  getOptionValue={getOptionValue}
+                  getOptionLabel={getOptionLabel}
+                  compact={true}
+                  readOnly={true}
+                />
+              </div>
+              <Button
+                {...tid.deleteSelected}
+                variant="tertiary"
+                label={""}
+                aria-label={`Delete selected ${otherProps.label}`}
+                icon={"x"}
+                onClick={() => {
+                  // Delete the selected value from the array
+                  const [selectedValues, selectedOptions] = options
+                    .filter((o) => values.filter((v) => v !== value).includes(getOptionValue(o)))
+                    .reduce(
+                      (acc, o) => {
+                        acc[0].push(getOptionValue(o));
+                        acc[1].push(o);
+                        return acc;
+                      },
+                      [[] as V[], [] as O[]],
+                    );
+
+                  onSelect(selectedValues, selectedOptions);
+                  setCurrentOptions(options.filter((o) => !selectedOptions.includes(o)));
+                  // Display the input field if there are no selected values
+                  if (selectedValues.length === 0) setIsDisplayed(true);
+                }}
+              />
+            </div>
+          );
+        })}
+        {isDisplayed && (
+          <div css={Css.mb1.$}>
+            <SelectField
+              {...tid.selectField}
+              label={otherProps.label}
+              labelStyle="hidden"
+              getOptionValue={getOptionValue}
+              getOptionLabel={getOptionLabel}
+              value={"" as string}
+              onSelect={(value) => {
+                onSelect([...values, value], options);
+                setCurrentOptions(currentOptions.filter((o) => getOptionValue(o) !== value));
+                setIsDisplayed(false);
+              }}
+              options={currentOptions}
+              disabled={otherProps.disabled}
+            />
+          </div>
+        )}
+        <Button
+          {...tid.addAnother}
+          label="Add Another"
+          variant="tertiary"
+          onClick={() => setIsDisplayed(true)}
+          disabled={isDisplayed || currentOptions.length === 0}
+        />
+      </div>
     </div>
   );
 }
