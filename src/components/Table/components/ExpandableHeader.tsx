5aca4c79d31556083d0ffee5de2f0a069a51084b
Kelsie Besinger Yeh <86630863+kbesingeryeh@users.noreply.github.com>
2023-01-04 10:08:33 -0600

    Add loader icon?

--- src/components/Table/components/ExpandableHeader.tsx
+++ src/components/Table/components/ExpandableHeader.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Icon } from "src/components/Icon";
 import { GridColumnWithId, Kinded, RenderAs } from "src/components/Table/types";
 import { TableStateContext } from "src/components/Table/utils/TableState";
@@ -19,7 +19,7 @@ export function ExpandableHeader<R extends Kinded>(props: ExpandableHeaderProps<
   const { tableState } = useContext(TableStateContext);
   const expandedColumnIds = useComputed(() => tableState.expandedColumnIds, [tableState]);
   const isExpanded = expandedColumnIds.includes(column.id);
-  // maybe create variable here to use w/the loading icon below
+  const [isLoading, setIsLoading] = useState(false);
   // Do not apply sticky styles when rendering as table. Currently the table does not properly respect column widths, causing the sticky offsets to be incorrect
   const applyStickyStyles = isExpanded && as !== "table";
   const { hoverProps, isHovered } = useHover({});
@@ -30,11 +30,15 @@ export function ExpandableHeader<R extends Kinded>(props: ExpandableHeaderProps<
       css={
         Css.df.xsMd.aic.jcsb.gap2.px1.hPx(32).mxPx(-8).w("calc(100% + 16px)").br4.lightBlue700.if(isHovered).bgGray100.$
       }
-      onClick={() =>
-        isFunction(column.expandColumns)
-          ? tableState.loadExpandedColumns(column)
-          : tableState.toggleExpandedColumn(column.id)
-      }
+      onClick={async () => {
+        if (isFunction(column.expandColumns)) {
+          setIsLoading(true);
+          await tableState.loadExpandedColumns(column);
+          setIsLoading(false);
+        } else {
+          return tableState.toggleExpandedColumn(column.id);
+        }
+      }}
       data-testid="expandableColumn"
     >
       <span
@@ -49,7 +53,7 @@ export function ExpandableHeader<R extends Kinded>(props: ExpandableHeaderProps<
         {title}
       </span>
       <span css={Css.if(applyStickyStyles).sticky.rightPx(12).z(zIndices.expandableHeaderIcon).$}>
-        <Icon icon={isExpanded ? "chevronLeft" : "chevronRight"} inc={2} />
+        <Icon icon={isExpanded ? "chevronLeft" : isLoading ? "loader" : "chevronRight"} inc={2} />
       </span>
     </button>
   );
