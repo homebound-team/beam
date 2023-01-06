bf99a74d5602f0a9e0e3b6a29934ff3f3638a24a
levi-huynh <levi_huynh@yahoo.com>
2023-01-06 13:49:32 -0800

    add global nav link menu

--- src/components/ButtonMenu.stories.tsx
+++ src/components/ButtonMenu.stories.tsx
@@ -227,6 +227,30 @@ export function IconMenuWtihSearchableList() {
   return <ButtonMenu defaultOpen trigger={{ icon: "archive" }} items={menuItems} searchable />;
 }
 
+export function GlobalNavLinkMenu() {
+  const menuItems: MenuItem[] = [
+    { label: "Page action", onClick: action("Test item clicked") },
+    { label: "Internal Link", onClick: "/fakeRoute" },
+    { label: "External Link - Homebound.com", onClick: "https://www.homebound.com" },
+  ];
+
+  return (
+    <div css={Css.mlPx(200).$}>
+      <h2 css={Css.lg.$}>Global Nav Link Menu</h2>
+      <ButtonMenu
+        trigger={{ navLabel: "Menu trigger", variant: "global" }}
+        items={menuItems}
+        persistentItems={[
+          { label: "Persistent Action", onClick: action("Persistent action clicked") },
+          { label: "Destructive Action", onClick: action("Destructive Action Clicked"), destructive: true },
+        ]}
+        defaultOpen
+        contrast={true}
+      />
+    </div>
+  );
+}
+
 export function MenuWithContrast() {
   const menuItems: MenuItem[] = [
     { label: "Page action", onClick: action("Test item clicked") },
