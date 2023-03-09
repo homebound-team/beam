298851c2cce0e17fb9aeffcc37484b8bf8c5e47d
Allan Galdino <agaldino@homebound.com>
2023-03-09 18:20:51 -0300

    feedback PR: init labelStyle prop from presentation context

--- src/forms/StaticField.tsx
+++ src/forms/StaticField.tsx
@@ -1,6 +1,6 @@
 import { useId } from "@react-aria/utils";
 import { ReactNode } from "react";
-import { PresentationFieldProps } from "src/components/PresentationContext";
+import { PresentationFieldProps, usePresentationContext } from "src/components/PresentationContext";
 import { Css, px } from "src/Css";
 import { defaultTestId } from "src/utils/defaultTestId";
 import { useTestIds } from "src/utils/useTestIds";
@@ -13,7 +13,8 @@ interface StaticFieldProps {
 }
 
 export function StaticField(props: StaticFieldProps) {
-  const { label, labelStyle = "above", value, children } = props;
+  const { fieldProps } = usePresentationContext();
+  const { label, labelStyle = fieldProps?.labelStyle ?? "above", value, children } = props;
   const tid = useTestIds(props, typeof label === "string" ? defaultTestId(label) : "staticField");
   const id = useId();
   return (
