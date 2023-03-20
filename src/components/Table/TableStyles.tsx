01855147fbaebeee6af7ab0ecb2c68b8487e09c3
levi-huynh <levi_huynh@yahoo.com>
2023-03-20 12:42:33 -0700

    clean up style

--- src/components/Table/TableStyles.tsx
+++ src/components/Table/TableStyles.tsx
@@ -116,7 +116,7 @@ function memoizedTableStyles() {
           ...Css.gray700.xsMd.bgGray200.aic.pxPx(12).whiteSpace("unset").hPx(40).$,
           ...(allWhite && Css.bgWhite.$),
         },
-        totalsCellCss: Css.bgWhite.gray700.bgGray100.xsMd.hPx(totalsRowHeight).pPx(12).bb.bl.bGray200.$,
+        totalsCellCss: Css.bgWhite.gray700.bgGray100.xsMd.hPx(totalsRowHeight).pPx(12).$,
         expandableHeaderCss: Css.bgWhite.gray900.xsMd.wsNormal
           .hPx(expandableHeaderRowHeight)
           .pxPx(12)
