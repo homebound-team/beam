7592e865f8514c1253950343dc73c521ab2e6852
Stephen Haberman <shaberman@homebound.com>
2023-05-22 14:20:26 -0500

    feat: Refactor Pagination component (#876)
    * Refactor Pagination.
    
    * Rename settings to page.
    
    * Tuplize the page prop.
    
    * Use limit instead of first.
    
    * Add offset/limit translation.
    
    * Use pageNumber in story.
    
    * Fix comment.

--- src/components/Pagination.stories.tsx
+++ src/components/Pagination.stories.tsx
@@ -1,11 +1,7 @@
 import { Meta } from "@storybook/react";
 import { useState } from "react";
-import { noop } from "src/utils";
-import { withBeamDecorator } from "src/utils/sb";
-import { FormLines, TextField } from "..";
-import { PageSettings, Pagination } from "./Pagination";
-
-const initPageSettings = { page: 1, perPage: 100 };
+import { FormLines, StaticField } from "..";
+import { Pagination } from "./Pagination";
 
 export default {
   title: "Workspace/Components/Pagination",
@@ -18,34 +14,29 @@ export default {
       url: "https://www.figma.com/file/aWUE4pPeUTgrYZ4vaTYZQU/%E2%9C%A8Beam-Design-System?node-id=3214%3A10519",
     },
   },
-  decorators: [withBeamDecorator],
 } as Meta;
 
 export function WithPages() {
-  return <RenderPagination totalRows={999} label="items" />;
+  return <RenderPagination totalRows={999} />;
 }
 
 export function NoPages() {
-  return <RenderPagination totalRows={0} label="items" />;
+  return <RenderPagination totalRows={0} />;
 }
 
 export function OnlyOnePage() {
   return <RenderPagination totalRows={14} />;
 }
 
-function RenderPagination({ totalRows, label }: { totalRows: number; label?: string }) {
-  const [settings, setSettings] = useState<PageSettings>({
-    page: initPageSettings.page,
-    perPage: initPageSettings.perPage,
-  });
-
+function RenderPagination({ totalRows }: { totalRows: number }) {
+  const page = useState({ pageNumber: 1, pageSize: 100 });
   return (
     <>
       <FormLines labelStyle="left">
-        <TextField label={"Number of items"} readOnly value={String(totalRows)} onChange={noop} />
-        <TextField label={"Current page"} readOnly value={String(settings.page)} onChange={noop} />
+        <StaticField label="Number of items" value={String(totalRows)} />
+        <StaticField label="Current page" value={String(page[0].pageNumber)} />
       </FormLines>
-      <Pagination totalCount={totalRows} label={label ?? "items"} setSettings={setSettings} settings={settings} />
+      <Pagination totalCount={totalRows} page={page} />
     </>
   );
 }
