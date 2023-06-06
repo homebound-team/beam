59d731638bc2eb997d42f74f6bfd2ad8ea6cbea1
Brandon <bdow@homebound.com>
2023-06-06 16:44:39 -0400

    fix: Add testid on DnDGridItemHandle (#885)

--- src/components/DnDGrid/DnDGridItemHandle.tsx
+++ src/components/DnDGrid/DnDGridItemHandle.tsx
@@ -1,5 +1,5 @@
 import { mergeProps, useFocusRing, useHover } from "react-aria";
-import { Css, Icon, IconKey, Palette } from "src";
+import { Css, Icon, IconKey, Palette, useTestIds } from "src";
 import { DnDGridItemProps } from "src/components/DnDGrid/useDnDGridItem";
 
 export interface DnDGridItemHandleProps {
@@ -14,6 +14,7 @@ export function DnDGridItemHandle(props: DnDGridItemHandleProps) {
   const { dragHandleProps, icon = "move", compact = false, color } = props;
   const { focusProps, isFocusVisible } = useFocusRing();
   const { hoverProps, isHovered } = useHover({});
+  const tid = useTestIds(props, "dragHandle");
 
   const iconButtonNormal = Css.hPx(28).wPx(28).br8.bw2.$;
   const iconButtonCompact = Css.hPx(18).wPx(18).br4.bw1.$;
@@ -27,6 +28,7 @@ export function DnDGridItemHandle(props: DnDGridItemHandleProps) {
         ...(isHovered && Css.bgGray200.$),
       }}
       {...mergeProps(dragHandleProps, focusProps, hoverProps)}
+      {...tid}
     >
       <Icon icon={icon} inc={compact ? 2 : undefined} color={color} />
     </button>
