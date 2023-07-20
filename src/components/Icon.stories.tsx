1acd70f2ec71aa564fe1b7e82e2794b470889d28
ipiedrahb <ipiedra@homebound.com>
2023-07-20 11:53:50 -0600

    feat: new icons - bath and car

--- src/components/Icon.stories.tsx
+++ src/components/Icon.stories.tsx
@@ -127,6 +127,8 @@ export const Icon = (props: IconProps) => {
     "sqFeet",
     "ruler",
     "palette",
+    "bath",
+    "garage",
   ];
   const navigationIcons: IconProps["icon"][] = [
     "projects",
