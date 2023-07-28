2173609c18d9b789edb0fadbe556424da7146137
Stephen Haberman <shaberman@homebound.com>
2023-07-28 08:46:00 -0500

    poc: Use useSyncExternalStore for useComputed.

--- src/hooks/useComputed.ts
+++ src/hooks/useComputed.ts
@@ -1,42 +1,55 @@
 import equal from "fast-deep-equal";
 import { autorun, IReactionDisposer } from "mobx";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useMemo, useRef } from "react";
+import { useSyncExternalStore } from "use-sync-external-store/shim";
 
 interface Current<T> {
+  notify: (() => void) | undefined;
   // Track the mobx autorunner
-  runner: IReactionDisposer | undefined;
+  dispose: IReactionDisposer | undefined;
   // Track the current value, as we only re-calc on changes
   value: T | undefined;
   // Track whether our autorun has actually run; sometimes it won't, i.e. if mobx
   // is already in a "run reactions" loop, we'll see useMemo complete before
   // we've had a chance to calc the value.
   hasRan: boolean;
+  version: number;
+  subscribe: (notify: () => void) => () => void;
+  getVersion: () => number;
 }
 
 /** Evaluates a computed function `fn` to a regular value and triggers a re-render whenever it changes. */
 export function useComputed<T>(fn: (prev: T | undefined) => T, deps: readonly any[]): T {
-  // We always return the useRef value, and use this just to trigger re-renders
-  const [, setTick] = useState(0);
-
-  const ref = useRef<Current<T>>({
-    runner: undefined,
-    value: undefined,
-    hasRan: false,
-  });
+  const ref = useRef<Current<T>>();
+  if (!ref.current) {
+    ref.current = {
+      notify: undefined,
+      dispose: undefined,
+      value: undefined,
+      hasRan: false,
+      version: 0,
+      getVersion() {
+        return this.version;
+      },
+      subscribe(notify: () => void) {
+        this.notify = notify;
+        return () => this.dispose?.();
+      },
+    };
+    ref.current.getVersion = ref.current.getVersion.bind(ref.current);
+    ref.current.subscribe = ref.current.subscribe.bind(ref.current);
+  }
 
   // We use a `useMemo` b/c we want this to synchronously calc, so that even
   // the very 1st render can use the result of our computed, i.e. instead of
   // with `useEffect`, which would only get calc'd after the 1st render has
   // already been done.
   useMemo(() => {
-    const { current } = ref;
-    // If deps has changed, unhook the previous observer
-    if (current.runner) {
-      current.runner();
-    }
+    const current = ref.current!;
     // If deps has changed, we're already re-running, so don't trigger a 2nd one
     current.hasRan = false;
-    current.runner = autorun(() => {
+    current.dispose?.();
+    current.dispose = autorun(() => {
       const { value: oldValue, hasRan: oldHasRun } = current;
       // Always eval fn() (even on 1st render) to register our observable.
       const newValue = fn(oldValue);
@@ -46,16 +59,15 @@ export function useComputed<T>(fn: (prev: T | undefined) => T, deps: readonly an
       // that if deps has changed, we're inherently in a re-render so also
       // don't need to trigger an additional re-render.
       if (oldHasRun && !equal(newValue, oldValue)) {
-        setTick((tick) => tick + 1);
+        current.version++;
+        current.notify?.();
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 
-  // unsubscribe the autorun when we're unmounted
-  useEffect(() => {
-    return ref.current.runner;
-  }, []);
+  const { current } = ref;
+  useSyncExternalStore(current.subscribe, current.getVersion, current.getVersion);
 
   // Occasionally autorun will not have run yet, in which case we have to just
   // accept running the eval fn twice (here to get the value for the 1st render,
