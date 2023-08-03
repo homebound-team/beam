788d697454546938258e265114730c96f556ab9e
Brandon Dow <bdow@homebound.com>
2023-08-03 15:56:54 -0400

    fix: Add special webkit styling to fix overflow issues on ios Safari

--- src/components/Layout/PreventBrowserScroll.tsx
+++ src/components/Layout/PreventBrowserScroll.tsx
@@ -5,7 +5,9 @@ import { ChildrenOnly } from "src/types";
 export function PreventBrowserScroll({ children }: ChildrenOnly) {
   return (
     // Take over the full viewport and hide any overflown content.
-    <div css={Css.overflowHidden.vh100.$}>
+    // Using `-webkit-fill-available`, otherwise `height: 100vh` includes the app bars in mobile Safari. See https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit/
+    // Setting the multiple "(min|max-)height" properties is necessary, as Truss will turn this into an object and there can only be one `height` property.
+    <div css={Css.overflowHidden.vh100.mh("-webkit-fill-available").maxh("-webkit-fill-available").$}>
       {/* Provides a fallback for scrolling the application if nested scrolling isn't needed, or forgotten */}
       <div css={Css.h100.df.fdc.mh0.overflowAuto.$}>{children}</div>
     </div>
