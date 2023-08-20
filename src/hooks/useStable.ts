3f0f5eefcda60cf23e3bd73c9a2ea3e987c43e49
Stephen Haberman <shaberman@homebound.com>
2023-08-20 11:28:31 -0500

    Re-order file.

--- src/hooks/useStable.ts
+++ src/hooks/useStable.ts
@@ -21,13 +21,19 @@ export type Stable<T> = T & { [stable]: true };
 /** Limits the dependencies of `useStable` to be themselves only stable values. */
 export type StableDep = Stable<unknown> | string | number | boolean | null | undefined;
 
-declare module "react" {
-  // Extend useMemo's return values to be automatically considered stable.
-  //
-  // With this extension, we could almost get away without our own `useStable` hook,
-  // but we can't use the extension mechanism to restrict useMemo's `deps` to be
-  // `ReadonlyArray<StableDep>`.
-  function useMemo<T>(factory: () => T, deps: ReadonlyArray<unknown> | undefined): Stable<T>;
+/**
+ * Allows creating "stable values", which is really just a dressed up `useMemo`.
+ *
+ * The only difference between `useStable` and `useMemo` is that `useStable` restricts its
+ * dependencies to themselves be stable values. This is similar to the eslint rule that
+ * catches a locally-declared `const array = [1, 2, 3]` can't be used as a dependency,
+ * because it will change on every render, but moves the check into the TypeScript type
+ * system where we can more robustly enforce it.
+ */
+export function useStable<T>(fn: () => T, deps: ReadonlyArray<StableDep> | undefined): Stable<T> {
+  // We don't need the `fn` factory to be a dep
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(fn, deps) as Stable<T>;
 }
 
 /**
@@ -46,17 +52,11 @@ export type UnstableProps<Props> = {
   [K in keyof Props]: Props[K] extends Stable<infer T> ? T : Props[K];
 };
 
-/**
- * Allows creating "stable values", which is really just a dressed up `useMemo`.
- *
- * The only difference between `useStable` and `useMemo` is that `useStable` restricts its
- * dependencies to themselves be stable values. This is similar to the eslint rule that
- * catches a locally-declared `const array = [1, 2, 3]` can't be used as a dependency,
- * because it will change on every render, but moves the check into the TypeScript type
- * system where we can more robustly enforce it.
- */
-export function useStable<T>(fn: () => T, deps: ReadonlyArray<StableDep> | undefined): Stable<T> {
-  // We don't need the `fn` factory to be a dep
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useMemo(fn, deps) as Stable<T>;
+declare module "react" {
+  // Extend useMemo's return values to be automatically considered stable.
+  //
+  // With this extension, we could almost get away without our own `useStable` hook,
+  // but we can't use the extension mechanism to restrict useMemo's `deps` to be
+  // `ReadonlyArray<StableDep>`.
+  function useMemo<T>(factory: () => T, deps: ReadonlyArray<unknown> | undefined): Stable<T>;
 }
