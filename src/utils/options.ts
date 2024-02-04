d8c118c7a5918ea7417142b5150a0240c5edb4a4
Stephen Haberman <shaberman@homebound.com>
2024-02-04 14:24:25 -0600

    Use in instead of ??.

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -2,11 +2,19 @@ import { fail } from "src/utils";
 
 // This `any` is currently on purpose to ignore type errors in ChipSelectField
 export function defaultOptionValue<O>(opt: O): any {
-  const o = opt as any;
-  return o.id ?? o.code ?? fail("Option has no id or code");
+  if (typeof opt !== "object" || !opt) fail(`Option ${opt} has no id or code`);
+  // Use `in` because returning undefined is fine
+  return "id" in opt ? opt.id : "code" in opt ? opt.code : fail(`Option ${JSON.stringify(opt)} has no id or code`);
 }
 
-export function defaultOptionLabel<O>(opt: O): string {
-  const o = opt as any;
-  return o.displayName ?? o.label ?? o.name ?? fail("Option has no displayName, label, or name");
+export function defaultOptionLabel<O>(opt: O): any {
+  if (typeof opt !== "object" || !opt) fail(`Option ${opt} has no id or code`);
+  // Use `in` because returning undefined is fine
+  return "displayName" in opt
+    ? opt.displayName
+    : "label" in opt
+    ? opt.label
+    : "name" in opt
+    ? opt.name
+    : fail(`Option ${JSON.stringify(opt)} has no displayName, label, or name`);
 }
