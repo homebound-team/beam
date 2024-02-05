a2514b24b6e58fb3d28a05a52270159204201b42
Stephen Haberman <shaberman@homebound.com>
2024-02-05 15:39:29 -0600

    fix: Prefer option.name if it exists.
    This keeps backwards compatibility.

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -10,11 +10,11 @@ export function defaultOptionValue<O>(opt: O): any {
 export function defaultOptionLabel<O>(opt: O): any {
   if (typeof opt !== "object" || !opt) fail(`Option ${opt} has no id or code`);
   // Use `in` because returning undefined is fine
-  return "displayName" in opt
+  return "name" in opt
+    ? opt.name
+    : "displayName" in opt
     ? opt.displayName
     : "label" in opt
     ? opt.label
-    : "name" in opt
-    ? opt.name
     : fail(`Option ${JSON.stringify(opt)} has no displayName, label, or name`);
 }
