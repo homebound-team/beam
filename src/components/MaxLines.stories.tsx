67fa3e91a171b26617711d542700ff75a39c05ec
Brandon <bdow@homebound.com>
2024-05-21 10:05:07 -0400

    feat: MaxLines component (#1028)
    MaxLines helps truncate long text vertically based on the number of
    lines the user wants to initially render. It provides a toggle to "Show
    More" / "Show Less".
    The component listens for changes to its size & children to ensure it
    updates its need for truncation accordingly.

---
+++ src/components/MaxLines.stories.tsx
@@ -0,0 +1,70 @@
+import { MaxLines } from "src/components/MaxLines";
+import { Button, Css } from "src";
+import { useMemo, useState } from "react";
+
+export default {
+  component: MaxLines,
+};
+
+export function Text() {
+  const shortContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+  const longContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a velit laoreet, ultricies lacus at, volutpat est. Vestibulum vel eleifend felis. In rhoncus elit id velit mattis, non dictum odio tincidunt.";
+  const [content, setContent] = useState(longContent);
+  const [width, setWidth] = useState(350);
+  return (
+    <div css={Css.df.fdc.gap2.aic.jcc.mtPx(200).$}>
+      <div css={Css.df.aic.gap2.$}>
+        <Button
+          onClick={() => setContent((prev) => (prev === longContent ? shortContent : longContent))}
+          label="Change content"
+        />
+        <Button onClick={() => setWidth((prev) => (prev === 350 ? 600 : 350))} label="Change width" />
+      </div>
+      <div css={Css.p1.ba.bGray400.bshBasic.br8.wPx(width).$}>
+        <MaxLines maxLines={3}>{content}</MaxLines>
+      </div>
+    </div>
+  );
+}
+
+export function WithElements() {
+  const longContent = useMemo(
+    () => (
+      <div css={Css.sm.$}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <span css={Css.baseSb.$}>
+          Vivamus a velit laoreet, ultricies lacus at, volutpat est.
+          <span css={Css.blue600.$}>
+            Vestibulum vel eleifend felis. In rhoncus elit id velit mattis, non dictum odio tincidunt.
+          </span>
+        </span>
+      </div>
+    ),
+    [],
+  );
+  const shortContent = useMemo(
+    () => (
+      <span css={Css.blue600.$}>
+        Vestibulum vel eleifend felis. In rhoncus elit id velit mattis, non dictum odio tincidunt.
+      </span>
+    ),
+    [],
+  );
+  const [content, setContent] = useState(longContent);
+  const [width, setWidth] = useState(350);
+  return (
+    <div css={Css.df.fdc.gap2.aic.jcc.mtPx(200).$}>
+      <div css={Css.df.aic.gap2.$}>
+        <Button
+          onClick={() => setContent((prev) => (prev === longContent ? shortContent : longContent))}
+          label="Change content"
+        />
+        <Button onClick={() => setWidth((prev) => (prev === 350 ? 600 : 350))} label="Change width" />
+      </div>
+      <div css={Css.p1.ba.bGray400.bshBasic.br8.wPx(width).$}>
+        <MaxLines maxLines={3}>{content}</MaxLines>
+      </div>
+    </div>
+  );
+}
