beac44460c99b6ab316c7a0e8be6d0da32dd5e7c
Brandon <bdow@homebound.com>
2024-06-03 19:25:16 -0400

    feat: Add 'map' icon (#1038)

--- src/components/Icon.stories.tsx
+++ src/components/Icon.stories.tsx
@@ -156,6 +156,7 @@ export const Icon = (props: IconProps) => {
     "updateDesignPackage",
     "exteriorStyle",
     "lockOpen",
+    "map",
   ];
   const navigationIcons: IconProps["icon"][] = [
     "projects",
