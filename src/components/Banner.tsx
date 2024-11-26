645a65bdd1024275a433d7230942a328d27e2ce0
Kelsie Besinger Yeh <kbesingeryeh@homebound.comm>
2024-11-26 09:56:25 -0600

    fix: Add optional prop to hide icon in Banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,16 +8,19 @@ export interface BannerProps {
   type: BannerTypes;
   message: ReactNode;
   onClose?: VoidFunction;
+  showIcon?: boolean;
 }
 
 export function Banner(props: BannerProps) {
-  const { type, message, onClose = false, ...others } = props;
+  const { type, message, onClose = false, showIcon = true, ...others } = props;
   const tid = useTestIds(others, "banner");
   return (
     <div css={{ ...variantStyles[type], ...Css.df.aic.w100.gap1.p2.gray900.base.bshBasic.$ }} {...tid} role="alert">
-      <span css={Css.fs0.$}>
-        <Icon icon={typeToIcon[type]} {...tid.type} color={Palette.Gray900} />
-      </span>
+      {showIcon && (
+        <span css={Css.fs0.$}>
+          <Icon icon={typeToIcon[type]} {...tid.type} color={Palette.Gray900} />
+        </span>
+      )}
       <span css={Css.fg1.$} {...tid.message}>
         {message}
       </span>
