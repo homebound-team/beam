ac368b74cffc88ac4947b83d814dc1113f0decc5
Kelsie Besinger Yeh <86630863+kbesingeryeh@users.noreply.github.com>
2025-01-13 14:43:53 -0600

    fix: SC-64282: Add back in early return for onSearch/debounce (#1101)
    A bug was reported on the `ChangeLog` page, where the project filter
    search was not working
    
    [SC-64282](https://app.shortcut.com/homebound-team/story/64282/projects-filter-not-working-properly-on-change-log)
    
    When I added back in the `debouncedSearch` early return, the project
    filter was fixed, but it caused a bug with the regular select field per
    the slack threads below and PRs
    * Slack Thread:
    https://homeboundinc.slack.com/archives/G01QEA5CXL1/p1734450733938479
    * PRs:
    *
    https://github.com/homebound-team/beam/pull/1017/files#diff-4cfc44d3175cb7b1fc2f316109423cede94d022b834134087a6a15bdbe4f296eL280
       * https://github.com/homebound-team/beam/pull/1098/files
    
    I still think this needs an early return, so I changed it to look at the
    `onSearch` prop and do an early return from that check.
    
    I tested on front-end and everything looks ok on the Change Log Page
    
    LOOM: https://www.loom.com/share/eefefa2904fb4d70b5515168cbad294a
    
    ---------
    
    Co-authored-by: Kelsie Besinger Yeh <kbesingeryeh@homebound.comm>

--- src/inputs/internal/ComboBoxBase.tsx
+++ src/inputs/internal/ComboBoxBase.tsx
@@ -306,6 +306,8 @@ export function ComboBoxBase<O, V extends Value>(props: ComboBoxBaseProps<O, V>)
 
   // Reset inputValue when closed or selected changes
   useEffect(() => {
+    // if we are using the onSearch functionality with debounceSearch, we don't want to reset the input value
+    if (onSearch && debouncedSearch) return;
     if (state.isOpen && multiselect) {
       // While the multiselect is open, let the user keep typing
       setFieldState((prevState) => ({
@@ -320,7 +322,16 @@ export function ComboBoxBase<O, V extends Value>(props: ComboBoxBaseProps<O, V>)
         inputValue: getInputValue(selectedOptions, getOptionLabel, multiselect, nothingSelectedText, isReadOnly),
       }));
     }
-  }, [state.isOpen, selectedOptions, getOptionLabel, multiselect, nothingSelectedText, isReadOnly]);
+  }, [
+    state.isOpen,
+    selectedOptions,
+    getOptionLabel,
+    multiselect,
+    nothingSelectedText,
+    isReadOnly,
+    onSearch,
+    debouncedSearch,
+  ]);
 
   // Call on search callback when the user types in the input field
   useEffect(() => {
