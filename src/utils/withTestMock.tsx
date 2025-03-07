2f628907eaa17923fd71de38cd6003c85129d313
Stephen Haberman <shaberman@homebound.com>
2025-03-06 20:31:22 -0600

    Let BoundDateField.test use the real component.

--- src/utils/withTestMock.tsx
+++ src/utils/withTestMock.tsx
@@ -1,10 +1,21 @@
 import { ComponentType } from "react";
 
+let useMock = process.env.NODE_ENV === "test";
+
+/**
+ * Toggles our mock DateField/RichTextField components.
+ *
+ * This defaults to true in test suites, but can be disabled if you
+ * _really_ want to test against the real component.
+ */
+export function setUseMockComponents(value: boolean) {
+  useMock = value;
+}
+
 // HOC that conditionally swaps components
 export function withTestMock<P extends object>(Component: ComponentType<P>, MockComponent: ComponentType<P>) {
   return (props: P) => {
-    const isTest = process.env.NODE_ENV === "test";
-    const SelectedComponent = isTest ? MockComponent : Component;
+    const SelectedComponent = useMock ? MockComponent : Component;
     return <SelectedComponent {...props} />;
   };
 }
