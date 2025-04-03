eb76952506b841c7cd1f8c43e905d7633c058424
Kimiko Kano <kkano@homebound.com>
2025-04-03 09:49:13 -0600

    Add transition in

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -17,28 +17,10 @@ export function RightSidebar({ content }: RightSidebarProps) {
   const [selectedIcon, setSelectedIcon] = useState<IconKey | undefined>(undefined);
   const tid = useTestIds({}, "rightSidebar");
 
-  if (!selectedIcon)
-    // display closed state
-    return (
-      <div css={Css.dg.mtPx(92).mx3.gap2.$}>
-        {content.map(({ icon }) => (
-          <div key={icon} css={Css.jse.$}>
-            <IconButton circle onClick={() => setSelectedIcon(icon)} icon={icon} inc={3.5} />
-          </div>
-        ))}
-      </div>
-    );
-
-  // display open state
   return (
-    <div css={Css.maxwPx(380).mr3.$}>
-      <div css={Css.df.jcsb.aic.$}>
-        <div css={Css.mlPx(-18).$}>
-          <IconButton circle onClick={() => setSelectedIcon(undefined)} icon="x" inc={3.5} />
-          {/* vertical line */}
-          <div css={Css.absolute.topPx(50).leftPx(6).h("calc(100vh - 50px)").wPx(1).bgGray300.$} />
-        </div>
-        <div css={Css.df.gap2.$}>
+    <>
+      <div css={Css.df.jcfe.relative.pr3.$}>
+        <div css={Css.df.gap2.z1.$}>
           {content.map(({ icon }) => (
             <IconButton
               key={`${icon}-${selectedIcon}`}
@@ -51,9 +33,24 @@ export function RightSidebar({ content }: RightSidebarProps) {
           ))}
         </div>
       </div>
-      <div css={Css.mt3.ml4.$} {...tid.content}>
-        {content.find((sidebar) => sidebar.icon === selectedIcon)?.render()}
+      <div
+        css={{
+          ...Css.wPx(selectedIcon ? 380 : 0).relative.topPx(-48).z0.$,
+          transition: selectedIcon ? "transform 300ms ease-in" : "transform 300ms ease-out",
+          transform: selectedIcon ? "translateX(0)" : "translateX(100%)",
+        }}
+      >
+        {selectedIcon && (
+          <div css={Css.absolute.leftPx(-24).top0.$}>
+            <IconButton circle onClick={() => setSelectedIcon(undefined)} icon="x" inc={3.5} />
+            {/* vertical line */}
+            <div css={Css.absolute.topPx(48).leftPx(23).h("calc(100vh - 168px)").wPx(2).bgGray300.$} />
+          </div>
+        )}
+        <div css={Css.ptPx(78).px3.$} {...tid.content}>
+          {content.find((sidebar) => sidebar.icon === selectedIcon)?.render()}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
