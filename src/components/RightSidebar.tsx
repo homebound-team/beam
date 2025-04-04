2af3027123c5434350668de32563bd3369e4f6cd
Kimiko Kano <kkano@homebound.com>
2025-04-04 08:42:28 -0600

    show icons vertically before opening sidebar

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { ReactNode, useState } from "react";
-import { Css } from "src/Css";
+import { Css, Palette } from "src/Css";
 import { useTestIds } from "src/utils";
 import { IconKey } from "./Icon";
 import { IconButton } from "./IconButton";
@@ -21,46 +21,65 @@ export function RightSidebar({ content }: RightSidebarProps) {
 
   return (
     <>
-      <div css={Css.df.jcfe.relative.pr3.$}>
-        <div css={Css.df.gap2.z1.$}>
-          {content.map(({ icon }) => (
-            <IconButton
-              // selectedIcon is added to key to reset the active state
-              key={`${icon}-${selectedIcon}`}
-              circle
-              active={icon === selectedIcon}
-              onClick={() => setSelectedIcon(icon)}
-              icon={icon}
-              inc={3.5}
-            />
-          ))}
-        </div>
-      </div>
-
+      <AnimatePresence>
+        {selectedIcon === undefined && (
+          // Display the icons vertically
+          <motion.div
+            key="iconsWithoutSidebar"
+            animate={{ transition: { delay: 0.2, opacity: 1 } }}
+            exit={{ opacity: 0 }}
+            css={Css.df.absolute.right0.pr3.$}
+            transition={{ ease: "linear", duration: 0.2 }}
+          >
+            <div css={Css.dg.gap2.z1.$}>
+              {content.map(({ icon }) => (
+                <IconButton
+                  key={icon}
+                  circle
+                  active={icon === selectedIcon}
+                  onClick={() => setSelectedIcon(icon)}
+                  icon={icon}
+                  inc={3.5}
+                />
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <AnimatePresence>
         {selectedIcon && (
           <motion.div
-            layout="position"
             key="rightSidebar"
-            data-testid="rightSidebarContent"
-            initial={{ x: width, position: "absolute" }}
-            animate={{ x: 0 }}
-            transition={{ ease: "linear", duration: 0.2 }}
+            initial={{ x: width, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.2, ease: "linear", duration: 0.2 }}
             exit={{ transition: { ease: "linear", duration: 0.2 }, x: width }}
+            css={Css.wPx(width).bl.bw1.bcGray300.absolute.z0.$}
           >
-            <div css={Css.wPx(width).relative.topPx(-48).z0.$}>
-              {selectedIcon && (
-                <>
-                  <div css={Css.absolute.leftPx(-24).top0.$}>
-                    <IconButton circle onClick={() => setSelectedIcon(undefined)} icon="x" inc={3.5} />
-                    {/* vertical line */}
-                    <div css={Css.absolute.topPx(48).leftPx(23).h("calc(100vh - 168px)").wPx(1).bgGray300.$} />
-                  </div>
-                  <div css={Css.ptPx(78).px3.$} {...tid.content}>
-                    {content.find((sidebar) => sidebar.icon === selectedIcon)?.render()}
-                  </div>
-                </>
-              )}
+            <div css={Css.px3.$} {...tid.content}>
+              <div css={Css.absolute.leftPx(-24).$}>
+                <IconButton
+                  bgColor={Palette.White}
+                  circle
+                  onClick={() => setSelectedIcon(undefined)}
+                  icon="x"
+                  inc={3.5}
+                />
+              </div>
+              <div css={Css.df.jcfe.gap2.pb3.z1.$}>
+                {content.map(({ icon }) => (
+                  <IconButton
+                    // selectedIcon is added to key to reset the active state
+                    key={`${icon}-${selectedIcon}`}
+                    circle
+                    active={icon === selectedIcon}
+                    onClick={() => setSelectedIcon(icon)}
+                    icon={icon}
+                    inc={3.5}
+                  />
+                ))}
+              </div>
+              {content.find((sidebar) => sidebar.icon === selectedIcon)?.render()}
             </div>
           </motion.div>
         )}
