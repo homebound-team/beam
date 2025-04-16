24613026ca04f6ac530e8653c398fb422dce766e
Kimiko Kano <kkano@homebound.com>
2025-04-16 11:39:40 -0600

    Update stories

--- src/components/ToggleChip.stories.tsx
+++ src/components/ToggleChip.stories.tsx
@@ -1,6 +1,8 @@
 import { action } from "@storybook/addon-actions";
 import { Meta } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/test";
 import { Css, PresentationProvider, ToggleChip } from "src/index";
+import { newStory } from "src/utils/sb";
 
 export default {
   component: ToggleChip,
@@ -12,30 +14,45 @@ export default {
   },
 } as Meta;
 
-export function DefaultToggleChip() {
-  return (
+export const DefaultToggleChip = newStory(
+  () => (
     <div css={Css.df.fdc.gap4.bgWhite.p2.$}>
-      <div css={Css.wPx(300).df.fdc.aifs.gap2.$}>
-        <ToggleChip text="Input chip" onClick={action("click")} />
-        <ToggleChip text="Input chip" icon="attachment" onClick={action("click")} />
-        <ToggleChip text="Disabled chip" disabled onClick={action("click")} />
-        <ToggleChip text="Filter chip" clearable={false} onClick={action("click")} />
-        <ToggleChip text="Filter chip" clearable={false} active={true} onClick={action("click")} />
-        <ToggleChip text="+2 more" clearable={false} active={true} onClick={action("click")} />
-        <ToggleChip text={"Input chip ".repeat(10)} icon="attachment" onClick={action("click")} />
+      <div>
+        <h2 css={Css.mb1.$}>Standard</h2>
+        <div css={Css.wPx(300).df.fdc.aifs.gap2.$}>
+          <ToggleChip text="Default" onClick={action("click")} />
+          <ToggleChip text="Icon" icon="attachment" onClick={action("click")} />
+          <ToggleChip text="Disabled" disabled onClick={action("click")} />
+          <ToggleChip text="Hovered" onClick={action("click")} />
+          <ToggleChip text="No x chip" clearable={false} onClick={action("click")} />
+          <ToggleChip text="Active" clearable={false} active={true} onClick={action("click")} />
+          <ToggleChip text={"Long text ".repeat(5)} icon="attachment" onClick={action("click")} />
+        </div>
       </div>
 
       <div>
         <h2 css={Css.mb1.$}>Compact</h2>
         <div css={Css.wPx(300).df.fdc.aifs.gap2.$}>
           <PresentationProvider fieldProps={{ compact: true }}>
-            <ToggleChip text="Input chip" onClick={action("click")} />
-            <ToggleChip text="Input chip" icon="attachment" onClick={action("click")} />
-            <ToggleChip text="Disabled Chip" disabled onClick={action("click")} />
-            <ToggleChip text={"Input chip ".repeat(10)} icon="attachment" onClick={action("click")} />
+            <ToggleChip text="Default" onClick={action("click")} />
+            <ToggleChip text="Icon" icon="attachment" onClick={action("click")} />
+            <ToggleChip text="Disabled" disabled onClick={action("click")} />
+            <ToggleChip text="Hovered" onClick={action("click")} />
+            <ToggleChip text="No x chip" clearable={false} onClick={action("click")} />
+            <ToggleChip text="Active" clearable={false} active={true} onClick={action("click")} />
+            <ToggleChip text={"Long text ".repeat(5)} icon="attachment" onClick={action("click")} />
           </PresentationProvider>
         </div>
       </div>
     </div>
-  );
-}
+  ),
+  {
+    play: async ({ canvasElement }) => {
+      const canvas = within(canvasElement);
+      const chip = canvas.getByText("Hovered chip");
+      return waitFor(async () => {
+        await userEvent.hover(chip);
+      });
+    },
+  },
+);
--- src/components/ToggleChip.test.tsx
+++ src/components/ToggleChip.test.tsx
@@ -1,3 +1,4 @@
+import { jest } from "@jest/globals";
 import { noop } from "src/utils";
 import { click, render } from "src/utils/rtl";
 import { ToggleChip } from "./ToggleChip";
@@ -5,6 +6,7 @@ import { ToggleChip } from "./ToggleChip";
 describe("ToggleChip", () => {
   it("renders", async () => {
     const r = await render(<ToggleChip text="Toggle me" onClick={noop} />);
+
     expect(r.chip.textContent).toBe("Toggle me");
     expect(r.chip_x).toBeInTheDocument();
   });
@@ -12,22 +14,28 @@ describe("ToggleChip", () => {
   it("handles click events", async () => {
     const onClick = jest.fn();
     const r = await render(<ToggleChip text="Click me" onClick={onClick} />);
-    await click(r.chip);
+    click(r.chip);
+
     expect(onClick).toHaveBeenCalled();
   });
 
   it("can be disabled", async () => {
     const r = await render(<ToggleChip text="Disabled" onClick={noop} disabled />);
+
     expect(r.chip).toBeDisabled();
+    // and the x icon is not displayed when disabled
+    expect(r.query.chip_x).not.toBeInTheDocument();
   });
 
   it("can render with an additional icon", async () => {
     const r = await render(<ToggleChip text="With icon" icon="attachment" onClick={noop} />);
+
     expect(r.chip_icon).toBeInTheDocument();
   });
 
   it("can be rendered without the x icon", async () => {
     const r = await render(<ToggleChip text="Without x icon" onClick={noop} clearable={false} />);
+
     expect(r.query.chip_x).not.toBeInTheDocument();
   });
 });
