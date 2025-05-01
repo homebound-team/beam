3f497d0217fb94a1ab8cbea659b84a0f3ba8a2e2
Ben Lambillotte <ben.lambillotte@gmail.com>
2025-04-30 17:39:22 -0700

    feat: [SC-68262] Grid table layout

---
+++ src/components/Layout/GridTableLayout/GridTableLayout.stories.tsx
@@ -0,0 +1,82 @@
+import { Meta } from "@storybook/react";
+import { useMemo } from "react";
+import { checkboxFilter, multiFilter } from "src/components/Filters";
+import { GridColumn } from "src/components/Table/types";
+import { simpleHeader, SimpleHeaderAndData } from "src/components/Table/utils/simpleHelpers";
+import { withBeamDecorator, withRouter } from "src/utils/sb";
+import { GridTableLayout as GridTableLayoutComponent, useGridTableLayoutState } from "./GridTableLayout";
+
+export default {
+  component: GridTableLayout,
+  decorators: [withBeamDecorator, withRouter()],
+} satisfies Meta;
+
+type Data = { name: string | undefined; value: number | undefined };
+type Row = SimpleHeaderAndData<Data>;
+
+export function GridTableLayout() {
+  const nameColumn: GridColumn<Row> = { header: "Name", data: ({ name }) => name };
+  const valueColumn: GridColumn<Row> = { header: "Value", data: ({ value }) => value };
+  const actionColumn: GridColumn<Row> = { header: "Action", data: () => <div>Actions</div> };
+
+  const filterDefs = useMemo(() => {
+    return {
+      primary: multiFilter({
+        options: [
+          { value: "primary", label: "Primary" },
+          { value: "secondary", label: "Secondary" },
+        ],
+        getOptionLabel: (tp) => tp.label,
+        getOptionValue: (tp) => tp.value,
+        label: "Preference",
+      }),
+      status: multiFilter({
+        options: [
+          { label: "Active", value: "active" },
+          { label: "Inactive", value: "inactive" },
+        ],
+        getOptionLabel: (cs) => cs.label,
+        getOptionValue: (cs) => cs.value,
+        label: "Status",
+      }),
+      needsRevision: checkboxFilter({
+        label: "Needs Revision",
+      }),
+    };
+  }, []);
+
+  const tableState = useGridTableLayoutState({
+    persistedFilter: {
+      filterDefs,
+      storageKey: "grid-table-layout",
+    },
+    useSearch: "client",
+  });
+
+  const { data } = useExampleQuery({ filter: tableState.filter });
+
+  return (
+    <GridTableLayoutComponent
+      pageTitle="Grid Table Layout Example"
+      breadcrumb={[
+        { href: "/", label: "Home" },
+        { href: "/", label: "Sub Page" },
+      ]}
+      tableState={tableState}
+      gridTableProps={{
+        columns: [nameColumn, valueColumn, actionColumn],
+        rows: [simpleHeader, ...data.map((d) => ({ kind: "data" as const, id: d.id, data: d }))],
+      }}
+    />
+  );
+}
+
+function useExampleQuery({ filter }: { filter: Record<string, unknown> }) {
+  return {
+    data: [
+      { id: "1", name: "a", value: 1 },
+      { id: "2", name: "b", value: 2 },
+      { id: "3", name: "c", value: 3 },
+    ],
+  };
+}
---
+++ src/components/Layout/GridTableLayout/GridTableLayout.tsx
@@ -0,0 +1,130 @@
+import { useEffect, useState } from "react";
+import { Filters } from "src/components/Filters/Filters";
+import { Icon } from "src/components/Icon";
+import { GridTable, GridTableProps } from "src/components/Table/GridTable";
+import { TableActions } from "src/components/Table/TableActions";
+import { GridTableXss, Kinded } from "src/components/Table/types";
+import { Css, Only, Palette } from "src/Css";
+import { usePersistedFilter, UsePersistedFilterProps } from "src/hooks";
+import { TextField } from "src/inputs/TextField";
+import { useTestIds } from "src/utils";
+import { useDebounce } from "use-debounce";
+import { FullBleed } from "../FullBleed";
+import { HeaderBreadcrumb, PageHeaderBreadcrumbs } from "../PageHeaderBreadcrumbs";
+import { ScrollableContent } from "../ScrollableContent";
+
+export type GridTableLayoutProps<
+  F extends Record<string, unknown>,
+  R extends Kinded,
+  X extends Only<GridTableXss, X>,
+> = {
+  pageTitle: string;
+  breadcrumb?: HeaderBreadcrumb | HeaderBreadcrumb[];
+  gridTableProps: GridTableProps<R, X>;
+  tableState?: ReturnType<typeof useGridTableLayoutState<F>>;
+};
+
+export function GridTableLayout<F extends Record<string, unknown>, R extends Kinded, X extends Only<GridTableXss, X>>(
+  props: GridTableLayoutProps<F, R, X>,
+) {
+  const { pageTitle, breadcrumb, gridTableProps, tableState } = props;
+  const { filterState, searchState } = tableState ?? {};
+
+  const clientSearch = searchState?.useSearch === "client" ? searchState.searchString : undefined;
+  const showTableActions = filterState || searchState;
+
+  const tids = useTestIds({}, "gridTableLayout");
+
+  return (
+    <div>
+      <Header pageTitle={pageTitle} breadcrumb={breadcrumb} {...tids} />
+      {showTableActions && (
+        <TableActions>
+          {searchState && <SearchBox onSearch={searchState.setSearchString} />}
+
+          {filterState && (
+            <Filters filterDefs={filterState.filterDefs} filter={filterState.filter} onChange={filterState.setFilter} />
+          )}
+        </TableActions>
+      )}
+      <ScrollableContent virtualized={gridTableProps.as === "virtual"}>
+        <GridTable {...gridTableProps} filter={clientSearch} style={{ allWhite: true }} />
+      </ScrollableContent>
+    </div>
+  );
+}
+
+export function useGridTableLayoutState<F extends Record<string, unknown>>({
+  persistedFilter,
+  useSearch,
+}: {
+  persistedFilter: UsePersistedFilterProps<F>;
+  useSearch?: "client" | "server";
+}) {
+  const { filter, setFilter } = usePersistedFilter<F>(persistedFilter);
+  const [searchString, setSearchString] = useState<string | undefined>("");
+
+  return {
+    filterState: { filter, setFilter, filterDefs: persistedFilter.filterDefs },
+    searchState: { searchString, setSearchString, useSearch },
+    // Duplicated for easier destructuring
+    filter,
+    searchString,
+  };
+}
+
+type HeaderProps = {
+  pageTitle: string;
+  breadcrumb?: HeaderBreadcrumb | HeaderBreadcrumb[];
+};
+
+function Header(props: HeaderProps) {
+  const { pageTitle, breadcrumb } = props;
+  const tids = useTestIds(props);
+
+  return (
+    <FullBleed>
+      <header css={{ ...Css.p3.mb3.mhPx(50).bgWhite.$ }} {...tids.header}>
+        {breadcrumb && <PageHeaderBreadcrumbs breadcrumb={breadcrumb} />}
+        <h1 css={Css.xl2Sb.mt2.$} {...tids.pageTitle}>
+          {pageTitle}
+        </h1>
+      </header>
+    </FullBleed>
+  );
+}
+
+type SearchBoxProps = {
+  onSearch(filter: string): void;
+  clearable?: boolean;
+  updateQueryString?: boolean;
+};
+
+function SearchBox(props: SearchBoxProps) {
+  const { onSearch, clearable, updateQueryString = true } = props;
+  // const [{ search: initialValue }, setQs] = useZodQueryString(searchSchema);
+  const [value, setValue] = useState<string>("");
+
+  // const tid = useTestIds(props, "searchBox");
+  const [debouncedSearch] = useDebounce(value, 300);
+
+  // TODO: figure out query strings
+  useEffect(() => {
+    onSearch(debouncedSearch);
+  }, [debouncedSearch, onSearch, updateQueryString]);
+
+  return (
+    <div css={Css.wPx(244).$}>
+      <TextField
+        label="Search"
+        labelStyle="hidden"
+        value={value}
+        onChange={(v) => setValue(v ?? "")}
+        placeholder={"Search"}
+        clearable={clearable}
+        startAdornment={<Icon icon="search" color={Palette.Gray700} />}
+        // {...tid}
+      />
+    </div>
+  );
+}
