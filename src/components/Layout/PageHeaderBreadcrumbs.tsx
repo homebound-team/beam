82a806b9872aeef8e1e4be438e31ea65af04a02e
Roberth Gómez <roberthdgg@gmail.com>
2026-01-08 18:34:06 -0300

    feat: [SC-79334] Add collapsible breadcrumbs to PageHeaderBreadcrumbs (#1144)
    Demo:
    
    
    https://github.com/user-attachments/assets/cc3d7bec-571d-4318-b118-48dea7ac204a
    
    ---------
    
    Co-authored-by: Roberth Gómez <roberthgomez@192.168.1.2>
    Co-authored-by: Roberth Gómez <roberthgomez@192.168.1.4>

--- src/components/Layout/PageHeaderBreadcrumbs.tsx
+++ src/components/Layout/PageHeaderBreadcrumbs.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, ReactNode, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { Css } from "src/Css";
 import { useTestIds } from "src/utils";
@@ -6,23 +6,45 @@ import { useTestIds } from "src/utils";
 export type HeaderBreadcrumb = {
   href: string;
   label: string;
+  right?: ReactNode;
 };
 
-export function PageHeaderBreadcrumbs({ breadcrumb }: { breadcrumb: HeaderBreadcrumb | HeaderBreadcrumb[] }) {
-  const tids = useTestIds({}, "pageHeaderBreadcrumbs");
+type PageHeaderBreadcrumbsProps = {
+  breadcrumb: HeaderBreadcrumb | HeaderBreadcrumb[];
+};
 
-  const breadcrumbs = Array.isArray(breadcrumb) ? breadcrumb : [breadcrumb];
+export function PageHeaderBreadcrumbs({ breadcrumb }: PageHeaderBreadcrumbsProps) {
+  const tids = useTestIds({}, "pageHeaderBreadcrumbs");
+  const breadcrumbs = useMemo(() => (Array.isArray(breadcrumb) ? breadcrumb : [breadcrumb]), [breadcrumb]);
+  const [collapsed, setCollapsed] = useState(true);
+
+  function renderBreadcrumb(bc: HeaderBreadcrumb, index: number, hideDivisor?: boolean) {
+    return (
+      // Adding index to key to prevent rendering issues when multiple items have the same label
+      <Fragment key={`${bc.label}-${index}`}>
+        {index > 0 && !hideDivisor && <span css={Css.smSb.gray700.mx1.myPx(2).$}>/</span>}
+        <Link to={bc.href} css={Css.smSb.gray700.onHover.gray900.$} className="navLink" {...tids.navLink}>
+          {bc.label}
+        </Link>
+        {bc.right}
+      </Fragment>
+    );
+  }
 
   return (
-    <div css={Css.df.aic.mbPx(4).$} {...tids}>
-      {breadcrumbs.map((breadcrumb, i) => (
-        <Fragment key={`bc-${breadcrumb.label}`}>
-          {i > 0 && <span css={Css.smSb.gray700.mr1.ml1.$}>/</span>}
-          <Link to={breadcrumb.href} css={Css.smSb.gray700.onHover.gray900.$} className="navLink" {...tids.navLink}>
-            {breadcrumb.label}
-          </Link>
-        </Fragment>
-      ))}
+    <div css={Css.df.aic.mbPx(4).$}>
+      {breadcrumbs.length > 3 && collapsed ? (
+        <>
+          {renderBreadcrumb(breadcrumbs[0], 0)}
+          <button {...tids.expand} css={Css.gray700.pxPx(8).$} onClick={() => setCollapsed(false)}>
+            ...
+          </button>
+          {renderBreadcrumb(breadcrumbs[breadcrumbs.length - 2], breadcrumbs.length - 2, true)}
+          {renderBreadcrumb(breadcrumbs[breadcrumbs.length - 1], breadcrumbs.length - 1)}
+        </>
+      ) : (
+        breadcrumbs.map((bc, i) => renderBreadcrumb(bc, i))
+      )}
     </div>
   );
 }
