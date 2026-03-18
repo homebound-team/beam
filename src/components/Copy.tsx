e65322f7ee6e4289e150c926f4e01605a28af1a9
Stephen Haberman <shaberman@homebound.com>
2026-03-17 23:50:30 -0500

    chore: Replace child selector with Copy.css file.

--- src/components/Copy.tsx
+++ src/components/Copy.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import { Css } from "src/Css";
+import "./Copy.css";
 
 export function Copy(props: { children: string | ReactNode }) {
   return (
     <div
+      className="beam-copy"
       css={{
         ...Css.sm.gray700.mt2.mb3.wPx(480).$,
-        "& > p": Css.my2.$,
       }}
     >
       {props.children}
