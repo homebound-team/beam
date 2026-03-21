cc321a360ce4aeac1b01391ffff765289c67007a
Stephen Haberman <shaberman@homebound.com>
2026-03-21 16:00:56 -0500

    Rewrite useResponsiveGridItem.

--- src/components/Grid/useResponsiveGrid.test.ts
+++ src/components/Grid/useResponsiveGrid.test.ts
@@ -2,16 +2,42 @@ import { renderHook } from "@testing-library/react";
 import { useResponsiveGrid } from "src/components/Grid/useResponsiveGrid";
 
 describe(useResponsiveGrid, () => {
-  it("defines the responsive grid styles", async () => {
+  it("defines the responsive grid styles", () => {
     const result = renderHook(() => useResponsiveGrid({ minColumnWidth: 100, gap: 10, columns: 4 })).result.current;
+
     expect(result).toMatchInlineSnapshot(`
       {
-        "gridStyles": {
-          "containerType": "inline-size",
-          "display": "grid",
-          "gap": "10px",
-          "gridTemplateColumns": "repeat(auto-fill, minmax(max(100px, calc((100% - 30px) / 4)), 1fr))",
-        },
+        "gridStyles": [
+          TrussDebugInfo {
+            "src": "useResponsiveGrid.ts:77",
+          },
+          {
+            "$$css": true,
+            "k1xSpc": "xrvj5dj",
+          },
+          [
+            {
+              "$$css": true,
+              "kumcoG": "xqketvx",
+            },
+            {
+              "--x-gridTemplateColumns": "repeat(auto-fill, minmax(max(100px, calc((100% - 30px) / 4)), 1fr))",
+            },
+          ],
+          {
+            "$$css": true,
+            "k9g6sI": "x12h1iku",
+          },
+          [
+            {
+              "$$css": true,
+              "kOIVth": "x1pidvrl",
+            },
+            {
+              "--x-gap": "10px",
+            },
+          ],
+        ],
       }
     `);
   });
