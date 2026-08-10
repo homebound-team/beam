f34b33138825ac605429a31ac8b39e9b89c5cc9e
Arthur Patterson <apatterson@homebound.com>
2026-08-10 17:29:31 -0500

    ai star icon

--- src/components/AiLoader.tsx
+++ src/components/AiLoader.tsx
@@ -1,4 +1,5 @@
 import { useId } from "react";
+import { aiStarPath } from "src/components/Icon";
 import { Css, increment, Margin, Palette, Xss } from "src/Css";
 import { useTestIds } from "src/utils/useTestIds";
 
@@ -38,7 +39,7 @@ export function AiLoader(props: AiLoaderProps) {
       </defs>
       {[0, 1, 2].map((i) => (
         <g key={i} transform={`translate(${starGap + i * (starSize + starGap)}, 0)`}>
-          <path d={starPath} fill={`url(#${gradientId})`} css={starStyles(i)} {...tid[`star${i + 1}`]} />
+          <path d={aiStarPath} fill={`url(#${gradientId})`} css={starStyles(i)} {...tid[`star${i + 1}`]} />
         </g>
       ))}
     </svg>
@@ -71,7 +72,3 @@ function starStyles(index: number) {
       .add("animationDelay", `${(index * cycleMs) / 3}ms`).$
   );
 }
-
-/** Eight-pointed sparkle, drawn to fill a 24x24 box. */
-const starPath =
-  "M12.9626 9.3548L19.4693 2.84806C19.8571 2.4603 20.4861 2.46022 20.8739 2.84806L20.9418 2.92322C21.2598 3.31316 21.2377 3.88891 20.8739 4.25269L14.0892 11.0374H23.0067C23.5552 11.0374 24 11.4822 24 12.0307C23.9999 12.5789 23.5553 13.024 23.0067 13.024H14.5159L20.6307 19.1387L20.6994 19.2147C20.9953 19.5785 20.9952 20.1029 20.6994 20.4666L20.6307 20.5426C20.2428 20.9304 19.6139 20.9304 19.226 20.5426L12.9626 14.2791V23.0067C12.9626 23.5552 12.5178 24 11.9693 24C11.4211 23.9999 10.976 23.5553 10.976 23.0067V14.1498L4.618 20.5078C4.25431 20.8715 3.67862 20.8941 3.28852 20.5757L3.21336 20.5078C2.82588 20.12 2.82584 19.4918 3.21336 19.104L9.29337 13.024H0.993265C0.47908 13.024 0.0559128 12.633 0.00484914 12.1325L0 12.0307C0 11.4822 0.444789 11.0374 0.993265 11.0374H9.7201L2.9701 4.28745C2.58225 3.89959 2.58241 3.2706 2.9701 2.88281L3.04526 2.81492C3.43535 2.49653 4.01105 2.51913 4.37473 2.88281L10.976 9.48411V0.993265C10.976 0.444731 11.4211 0.000149661 11.9693 0C12.5178 2.4664e-08 12.9626 0.444789 12.9626 0.993265V9.3548Z";
